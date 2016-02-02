7163cedc8ac84bd467398f259ea0fd4984ae8fbe
Jared <jaredlll08@gmail.com>
2016-02-02 11:14:16 +0200

    Close #282

--- MineTweaker3-API/src/main/java/minetweaker/api/item/IngredientStack.java
+++ MineTweaker3-API/src/main/java/minetweaker/api/item/IngredientStack.java
@@ -1,7 +1,9 @@
 package minetweaker.api.item;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import minetweaker.api.liquid.ILiquidStack;
 import minetweaker.api.player.IPlayer;
 
@@ -12,86 +14,103 @@ import minetweaker.api.player.IPlayer;
  * @author Stan Hebben
  */
 public class IngredientStack implements IIngredient {
-	private final IIngredient ingredient;
-	private final int amount;
+    private final IIngredient ingredient;
+    private final int amount;
 
-	public IngredientStack(IIngredient ingredient, int amount) {
-		this.ingredient = ingredient;
-		this.amount = amount;
-	}
+    public IngredientStack(IIngredient ingredient, int amount) {
+        this.ingredient = ingredient;
+        this.amount = amount;
+    }
 
-	@Override
-	public String getMark() {
-		return null;
-	}
+    @Override
+    public String getMark() {
+        return null;
+    }
 
-	@Override
-	public int getAmount() {
-		return amount;
-	}
+    @Override
+    public int getAmount() {
+        return amount;
+    }
 
-	@Override
-	public List<IItemStack> getItems() {
-		return ingredient.getItems();
-	}
+    @Override
+    public List<IItemStack> getItems() {
+        return ingredient.getItems();
+    }
 
-	@Override
-	public List<ILiquidStack> getLiquids() {
-		return Collections.emptyList();
-	}
+    @Override
+    public List<ILiquidStack> getLiquids() {
+        return Collections.emptyList();
+    }
 
-	@Override
-	public IIngredient amount(int amount) {
-		return new IngredientStack(ingredient, amount);
-	}
+    @Override
+    public IIngredient amount(int amount) {
+        return new IngredientStack(ingredient, amount);
+    }
 
-	@Override
-	public IIngredient transform(IItemTransformer transformer) {
-		return new IngredientStack(ingredient.transform(transformer), amount);
-	}
+    @Override
+    public IIngredient transform(IItemTransformer transformer) {
+        return new IngredientStack(ingredient.transform(transformer), amount);
+    }
 
-	@Override
-	public IIngredient only(IItemCondition condition) {
-		return new IngredientStack(ingredient.only(condition), amount);
-	}
+    @Override
+    public IIngredient only(IItemCondition condition) {
+        return new IngredientStack(ingredient.only(condition), amount);
+    }
 
-	@Override
-	public IIngredient marked(String mark) {
-		return new IngredientStack(ingredient.marked(mark), amount);
-	}
+    @Override
+    public IIngredient marked(String mark) {
+        return new IngredientStack(ingredient.marked(mark), amount);
+    }
 
-	@Override
-	public IIngredient or(IIngredient ingredient) {
-		return new IngredientOr(this, ingredient);
-	}
+    @Override
+    public IIngredient or(IIngredient ingredient) {
+        return new IngredientOr(this, ingredient);
+    }
 
-	@Override
-	public boolean matches(IItemStack item) {
-		return item.getAmount() == amount && ingredient.matches(item);
-	}
+    @Override
+    public boolean matches(IItemStack item) {
+        return item.getAmount() == amount && ingredient.matches(item);
+    }
 
-	@Override
-	public boolean matches(ILiquidStack liquid) {
-		return false;
-	}
+    @Override
+    public boolean matches(ILiquidStack liquid) {
+        return false;
+    }
 
-	@Override
-	public boolean contains(IIngredient ingredient) {
-		return this.ingredient.contains(ingredient);
-	}
+    @Override
+    public boolean contains(IIngredient ingredient) {
+        return this.ingredient.contains(ingredient);
+    }
 
-	@Override
-	public IItemStack applyTransform(IItemStack item, IPlayer byPlayer) {
-		return ingredient.applyTransform(item, byPlayer);
-	}
+    @Override
+    public IItemStack applyTransform(IItemStack item, IPlayer byPlayer) {
+        return ingredient.applyTransform(item, byPlayer);
+    }
 
-	@Override
-	public Object getInternal() {
-		return null;
-	}
+    @Override
+    public Object getInternal() {
+        return null;
+    }
 
-	@Override
-	public boolean hasTransformers() {
-		return ingredient.hasTransformers();
-	}
+    @Override
+    public boolean hasTransformers() {
+        return ingredient.hasTransformers();
+    }
+
+    // #############################
+    // ### Object implementation ###
+    // #############################
+
+    @Override
+    public String toString() {
+        String s = "(Ingredients) ";
+        List<String> names = new ArrayList<String>();
+
+        for (IItemStack st : ingredient.getItems()) {
+            names.add(st.getName());
+        }
+        s = s.join(",", names);
+
+        return s;
+    }
 }
