873bff3a11d59e93ef5ee026f297710807b9c6fa
Jared <jaredlll08@gmail.com>
2016-04-26 00:06:14 +0200

    MC19

---
+++ MineTweaker3-MC19-Main/src/main/java/minetweaker/mc19/formatting/FormattedMarkupString.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package minetweaker.mc19.formatting;
+
+import minetweaker.api.formatting.IFormattedText;
+import net.minecraft.util.text.TextFormatting;
+
+/**
+ * @author Stan
+ */
+public class FormattedMarkupString implements IMCFormattedString{
+    private final TextFormatting markup;
+    private final IMCFormattedString contents;
+
+    public FormattedMarkupString(TextFormatting markup, IMCFormattedString contents){
+        this.markup = markup;
+        this.contents = contents;
+    }
+
+    @Override
+    public String getTooltipString(){
+        return markup + contents.getTooltipString(markup.toString());
+    }
+
+    @Override
+    public String getTooltipString(String context){
+        return markup + contents.getTooltipString(context + markup);
+    }
+
+    @Override
+    public IFormattedText add(IFormattedText other){
+        return cat(other);
+    }
+
+    @Override
+    public IFormattedText cat(IFormattedText other){
+        return new FormattedStringJoin(this, (IMCFormattedString) other);
+    }
+}
---
+++ MineTweaker3-MC19-Main/src/main/java/minetweaker/mc19/formatting/IMCFormattedString.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package minetweaker.mc19.formatting;
+
+import minetweaker.api.formatting.IFormattedText;
+
+/**
+ * @author Stan
+ */
+public interface IMCFormattedString extends IFormattedText{
+    String getTooltipString();
+
+    String getTooltipString(String context);
+}
