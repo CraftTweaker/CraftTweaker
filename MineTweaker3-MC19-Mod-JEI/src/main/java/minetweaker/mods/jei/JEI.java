566e0fd9f177d67e306f0da6baf7e305398ba23f
Jared <jaredlll08@gmail.com>
2016-05-04 20:37:26 +0200

    Closed #11
    Very hacky, but it works

--- MineTweaker3-MC19-Mod-JEI/src/main/java/minetweaker/mods/jei/JEI.java
+++ MineTweaker3-MC19-Mod-JEI/src/main/java/minetweaker/mods/jei/JEI.java
@@ -2,17 +2,20 @@ package minetweaker.mods.jei;
 
 import minetweaker.IUndoableAction;
 import minetweaker.MineTweakerAPI;
+import minetweaker.MineTweakerImplementationAPI;
 import minetweaker.api.item.IItemStack;
+import minetweaker.util.IEventHandler;
 import stanhebben.zenscript.annotations.NotNull;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
 import static minetweaker.api.minecraft.MineTweakerMC.getItemStack;
+import static minetweaker.mods.jei.JEIAddonPlugin.jeiHelpers;
 
 /**
- * MineTweaker NEI support.
+ * MineTweaker JEI support.
  * 
- * Enables hiding NEI items as well as adding new item stacks. These item stacks
+ * Enables hiding JEI items as well as adding new item stacks. These item stacks
  * can then show a custom message or contain NBT data. Can be used to show a
  * custom message or lore with certain items, or to provide spawnable items with
  * specific NBT tags.
@@ -21,8 +24,9 @@ import static minetweaker.api.minecraft.MineTweakerMC.getItemStack;
  */
 @ZenClass("mods.jei.JEI")
 public class JEI {
+
 	/**
-	 * Hides a specific item in NEI. Will take into account metadata values, if
+	 * Hides a specific item in JEI. Will take into account metadata values, if
 	 * any.
 	 * 
 	 * @param item
@@ -32,77 +36,6 @@ public class JEI {
 	public static void hide(@NotNull IItemStack item) {
 		MineTweakerAPI.apply(new JEIHideItemAction(item));
 	}
-	//
-	// /**
-	// * Adds a NEI entry. The item stack can contain damage values and NBT
-	// tags.
-	// *
-	// * @param stack
-	// * item stack to be added
-	// */
-	// @ZenMethod
-	// public static void addEntry(@NotNull IItemStack stack) {
-	// MineTweakerAPI.apply(new NEIAddEntryAction(getItemStack(stack)));
-	// }
-
-	// /**
-	// * Overrides a name in NEI. Note that this will not change the original
-	// * display name, but it will change the way it is displayed in NEI.
-	// *
-	// * @param item
-	// * item
-	// * @param name
-	// * item name
-	// */
-	// @ZenMethod
-	// public static void overrideName(@NotNull IItemStack item, @NotNull String
-	// name) {
-	// MineTweakerAPI.apply(new NEIOverrideNameAction(getItemStack(item),
-	// name));
-	// }
-
-	// #############################
-	// ### Private inner classes ###
-	// #############################
-
-	// private static class NEIAddEntryAction implements IUndoableAction {
-	// private final ItemStack item;
-	//
-	// public NEIAddEntryAction(ItemStack item) {
-	// this.item = item;
-	// }
-	//
-	// @Override
-	// public void apply() {
-	// JEIAddonPlugin.jeiHelpers.getItemBlacklist().addItemToBlacklist(item);
-	// // API.addItemListEntry(item);
-	// }
-	//
-	// @Override
-	// public boolean canUndo() {
-	// return true;
-	// }
-	//
-	// @Override
-	// public void undo() {
-	// API.hideItem(item);
-	// }
-	//
-	// @Override
-	// public String describe() {
-	// return "Adding " + item.getDisplayName() + " as NEI entry";
-	// }
-	//
-	// @Override
-	// public String describeUndo() {
-	// return "Removing " + item.getDisplayName() + " as NEI entry";
-	// }
-	//
-	// @Override
-	// public Object getOverrideKey() {
-	// return null;
-	// }
-	// }
 
 	private static class JEIHideItemAction implements IUndoableAction {
 		private final IItemStack stack;
@@ -113,12 +46,7 @@ public class JEI {
 
 		@Override
 		public void apply() {
-			System.out.println(stack);
-			System.out.println(getItemStack(stack));
-            System.out.println(JEIAddonPlugin.jeiHelpers);
-			System.out.println(JEIAddonPlugin.jeiHelpers.getItemBlacklist());
-
-			JEIAddonPlugin.jeiHelpers.getItemBlacklist().addItemToBlacklist(getItemStack(stack));
+			jeiHelpers.getItemBlacklist().addItemToBlacklist(getItemStack(stack));
 		}
 
 		@Override
@@ -128,7 +56,7 @@ public class JEI {
 
 		@Override
 		public void undo() {
-			JEIAddonPlugin.jeiHelpers.getItemBlacklist().removeItemFromBlacklist(getItemStack(stack));
+			jeiHelpers.getItemBlacklist().removeItemFromBlacklist(getItemStack(stack));
 		}
 
 		@Override
@@ -146,30 +74,4 @@ public class JEI {
 			return null;
 		}
 	}
-
-	// private static class NEIOverrideNameAction extends OneWayAction {
-	// private final ItemStack item;
-	// private final String name;
-	//
-	// public NEIOverrideNameAction(ItemStack item, String name) {
-	// this.item = item;
-	// this.name = name;
-	// }
-	//
-	// @Override
-	// public void apply() {
-	// API.setOverrideName(item, name);
-	// }
-	//
-	// @Override
-	// public String describe() {
-	// return "Overriding NEI item name of " + item.getUnlocalizedName() + " to
-	// " + name;
-	// }
-	//
-	// @Override
-	// public Object getOverrideKey() {
-	// return item;
-	// }
-	// }
 }
