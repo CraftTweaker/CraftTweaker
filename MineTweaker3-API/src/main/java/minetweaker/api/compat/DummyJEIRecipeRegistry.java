0258c99e82b2b5f418d54e63165417c63175a8e8
Jared <jaredlll08@gmail.com>
2017-02-15 00:24:52 +0200

    Added debugging to the dummyJEI registry

--- MineTweaker3-API/src/main/java/minetweaker/api/compat/DummyJEIRecipeRegistry.java
+++ MineTweaker3-API/src/main/java/minetweaker/api/compat/DummyJEIRecipeRegistry.java
@@ -6,19 +6,21 @@ public class DummyJEIRecipeRegistry implements IJEIRecipeRegistry {
 	
 	@Override
 	public void addRecipe(Object object) {
+		System.out.println("adding recipe " + object);
 	}
 	
 	@Override
 	public void removeRecipe(Object object) {
+		System.out.println("Removing Recipe" + object);
 	}
 	
 	@Override
 	public void addFurnace(List<Object> inputs, Object output) {
-		
+		System.out.println("adding furnace " + inputs + ":" + output);
 	}
 	
 	@Override
 	public void removeFurnace(Object object) {
-		
+		System.out.println("Removing Furnace" + object);
 	}
 }
