48a5c9fa03635eef8ee7e750a880a49b6d761246
Jared <jaredlll08@gmail.com>
2017-08-17 12:03:33 +0200

    Close #302

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/player/MCPlayer.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/player/MCPlayer.java
@@ -9,60 +9,66 @@ import crafttweaker.api.player.IPlayer;
 import crafttweaker.mc1120.CraftTweaker;
 import crafttweaker.mc1120.data.NBTConverter;
 import crafttweaker.mc1120.network.*;
+import net.minecraft.command.*;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.*;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.text.*;
 
 /**
  * @author Stan
  */
 public class MCPlayer implements IPlayer {
-
+    
     private final EntityPlayer player;
-
+    
     public MCPlayer(EntityPlayer player) {
         this.player = player;
     }
-
+    
     public EntityPlayer getInternal() {
         return player;
     }
-
+    
     @Override
     public String getId() {
         return player.getUniqueID().toString();
     }
-
+    
     @Override
     public String getName() {
         return player.getName();
     }
-
+    
     @Override
     public IData getData() {
         return NBTConverter.from(player.getEntityData(), true);
     }
-
+    
     @Override
     public int getXP() {
         return player.experienceLevel;
     }
-
+    
     @Override
     public void setXP(int xp) {
         player.addExperienceLevel(-player.experienceLevel);
         player.addExperienceLevel(xp);
     }
-
+    
     @Override
     public void removeXP(int xp) {
         player.addExperience(-xp);
     }
-
+    
     @Override
     public void update(IData data) {
         NBTConverter.updateMap(player.getEntityData(), data);
     }
-
+    
     @Override
     public void sendChat(IChatMessage message) {
         Object internal = message.getInternal();
@@ -72,77 +78,99 @@ public class MCPlayer implements IPlayer {
         }
         player.sendMessage((ITextComponent) internal);
     }
-
+    
     @Override
     public void sendChat(String message) {
         player.sendMessage(new TextComponentString(message));
     }
-
+    
     @Override
     public int getHotbarSize() {
         return 9;
     }
-
+    
     @Override
     public IItemStack getHotbarStack(int i) {
         return i < 0 || i >= 9 ? null : CraftTweakerMC.getIItemStack(player.inventory.getStackInSlot(i));
     }
-
+    
     @Override
     public int getInventorySize() {
         return player.inventory.getSizeInventory();
     }
-
+    
     @Override
     public IItemStack getInventoryStack(int i) {
         return CraftTweakerMC.getIItemStack(player.inventory.getStackInSlot(i));
     }
-
+    
     @Override
     public IItemStack getCurrentItem() {
         return CraftTweakerMC.getIItemStack(player.inventory.getCurrentItem());
     }
-
+    
     @Override
     public boolean isCreative() {
         return player.capabilities.isCreativeMode;
     }
-
+    
     @Override
     public boolean isAdventure() {
         return !player.capabilities.allowEdit;
     }
-
+    
     @Override
     public void openBrowser(String url) {
         if(player instanceof EntityPlayerMP) {
             CraftTweaker.NETWORK.sendTo(new MessageOpenBrowser(url), (EntityPlayerMP) player);
         }
     }
-
+    
     @Override
     public void copyToClipboard(String value) {
         if(player instanceof EntityPlayerMP) {
             CraftTweaker.NETWORK.sendTo(new MessageCopyClipboard(value), (EntityPlayerMP) player);
         }
     }
-
+    
     @Override
     public boolean equals(Object other) {
         return other.getClass() == this.getClass() && ((MCPlayer) other).player == player;
-
+        
     }
-
+    
     @Override
     public int hashCode() {
         int hash = 5;
         hash = 23 * hash + (this.player != null ? this.player.hashCode() : 0);
         return hash;
     }
-
+    
     @Override
     public void give(IItemStack stack) {
-        player.inventory.addItemStackToInventory(CraftTweakerMC.getItemStack(stack).copy());
+        ItemStack itemstack = CraftTweakerMC.getItemStack(stack).copy();
+        boolean flag = player.inventory.addItemStackToInventory(itemstack);
+        
+        if(flag) {
+            player.world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.2F, ((player.getRNG().nextFloat() - player.getRNG().nextFloat()) * 0.7F + 1.0F) * 2.0F);
+            player.inventoryContainer.detectAndSendChanges();
+        }
+        
+        if(flag && itemstack.isEmpty()) {
+            itemstack.setCount(1);
+            EntityItem entityitem1 = player.dropItem(itemstack, false);
+            
+            if(entityitem1 != null) {
+                entityitem1.makeFakeItem();
+            }
+        } else {
+            EntityItem entityitem = player.dropItem(itemstack, false);
+            
+            if(entityitem != null) {
+                entityitem.setNoPickupDelay();
+                entityitem.setOwner(player.getName());
+            }
+        }
     }
     
     @Override
