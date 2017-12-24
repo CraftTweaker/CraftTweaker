a007b7a2d505a22824c8072dda04d15f68d55125
Jared <jaredlll08@gmail.com>
2017-12-24 17:03:29 +0200

    Close #404

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/actions/ActionOreDictRemoveItem.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/actions/ActionOreDictRemoveItem.java
@@ -1,24 +1,38 @@
 package crafttweaker.mc1120.actions;
 
-import crafttweaker.IAction;
+import crafttweaker.*;
+import crafttweaker.api.item.IItemStack;
 import crafttweaker.mc1120.oredict.MCOreDictEntry;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
+import static crafttweaker.api.minecraft.CraftTweakerMC.getIItemStackWildcardSize;
+
 public class ActionOreDictRemoveItem implements IAction {
     
     private final String id;
-    private final ItemStack item;
+    private final IItemStack item;
     
-    public ActionOreDictRemoveItem(String id, ItemStack item) {
+    public ActionOreDictRemoveItem(String id, IItemStack item) {
         this.id = id;
         this.item = item;
     }
     
     @Override
     public void apply() {
+        ItemStack result = ItemStack.EMPTY;
+        for(ItemStack itemStack : OreDictionary.getOres(id)) {
+            if(item.matches(getIItemStackWildcardSize(itemStack))) {
+                result = itemStack;
+                break;
+            }
+        }
         int oreId = OreDictionary.getOreID(id);
-        MCOreDictEntry.getOredictContents().get(oreId).remove(item);
+        if(result.isEmpty()) {
+            CraftTweakerAPI.logError(item + " does not exist in oredict: " + id);
+            return;
+        }
+        MCOreDictEntry.getOredictContents().get(oreId).remove(result);
     }
     
     
