c416eee016010dea4a2bbda656e5b8d42270e44f
kindlich <timo-klenk@web.de>
2018-01-02 00:29:35 +0100

    Added more compare and rotateY to IFacing

--- CraftTweaker2-API/src/main/java/crafttweaker/api/world/IFacing.java
+++ CraftTweaker2-API/src/main/java/crafttweaker/api/world/IFacing.java
@@ -1,20 +1,27 @@
 package crafttweaker.api.world;
 
 import crafttweaker.annotations.ZenRegister;
-import stanhebben.zenscript.annotations.ZenClass;
-import stanhebben.zenscript.annotations.ZenGetter;
-import stanhebben.zenscript.annotations.ZenMethod;
+import stanhebben.zenscript.annotations.*;
 
 @ZenClass("crafttweaker.world.IFacing")
 @ZenRegister
 public interface IFacing {
-	
-	@ZenMethod
-	IFacing opposite();
-	
-	@ZenGetter("name")
-	@ZenMethod
-	String getName();
-	
-	Object getInternal();
+    
+    @ZenMethod
+    @ZenGetter("opposite")
+    IFacing opposite();
+    
+    @ZenGetter("name")
+    @ZenMethod
+    String getName();
+    
+    Object getInternal();
+    
+    @ZenOperator(OperatorType.COMPARE)
+    @ZenMethod
+    int compare(IFacing other);
+    
+    @ZenMethod
+    @ZenGetter("rotateY")
+    IFacing rotateY();
 }
