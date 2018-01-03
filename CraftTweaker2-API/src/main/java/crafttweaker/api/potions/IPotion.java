b9e0a976316e09e02208f5a93bb918d4b4200ef8
kindlich <timo-klenk@web.de>
2018-01-03 23:34:32 +0100

    Added new Methods to IPotion, Added IPotionEffect

--- CraftTweaker2-API/src/main/java/crafttweaker/api/potions/IPotion.java
+++ CraftTweaker2-API/src/main/java/crafttweaker/api/potions/IPotion.java
@@ -1,8 +1,11 @@
 package crafttweaker.api.potions;
 
+import crafttweaker.api.item.IItemStack;
 import stanhebben.zenscript.annotations.*;
 
-@ZenClass("crafttweaker.api.IPotion")
+import java.util.List;
+
+@ZenClass("crafttweaker.potions.IPotion")
 public interface IPotion {
     
     @ZenGetter("name")
@@ -16,6 +19,24 @@ public interface IPotion {
     
     @ZenGetter("liquidColour")
     int getLiquidColour();
+    
+    @ZenMethod
+    IPotionEffect makePotionEffect(int duration, int amplifier);
+    
+    @ZenMethod
+    IPotionEffect makePotionEffect(int duration, int amplifier, boolean ambient, boolean showParticles);
+    
+    @ZenGetter("curativeItems")
+    List<IItemStack> getCurativeItems();
+    
+    @ZenGetter
+    boolean hasStatusIcon();
+    
+    @ZenGetter
+    boolean isBeneficial();
+    
+    @ZenGetter
+    boolean isInstant();
     
     Object getInternal();
 }
