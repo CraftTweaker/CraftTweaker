919f6e14c2b60ec4900643b75d28c16f850382c7
Jared <jaredlll08@gmail.com>
2018-01-10 03:43:34 +0200

    Fixes localizing crashing on servers (clsoes #416)

--- CraftTweaker2-API/src/main/java/crafttweaker/api/block/IBlockDefinition.java
+++ CraftTweaker2-API/src/main/java/crafttweaker/api/block/IBlockDefinition.java
@@ -23,8 +23,8 @@ public interface IBlockDefinition {
     
     Object getInternal();
     
-    @ZenSetter("lightOpacy")
-    void setLightOpacy(int lightOpacy);
+    @ZenSetter("lightOpacity")
+    void setLightOpacity(int lightOpacity);
     
     @ZenSetter("lightLevel")
     void setLightLevel(float lightLevel);
