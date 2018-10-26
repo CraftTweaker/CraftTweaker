3641c279a3e254790b170c3524d192304210122a
Jared <jaredlll08@gmail.com>
2018-10-26 02:17:45 +0200

    Close #642

---
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/events/ActionApplyEvent.java
@@ -0,0 +1,17 @@
+package crafttweaker.mc1120.events;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+public class ActionApplyEvent extends Event {
+    public static class Pre extends ActionApplyEvent {
+        public Pre() {
+        
+        }
+    }
+    
+    public static class Post extends ActionApplyEvent {
+        public Post() {
+        
+        }
+    }
+}
