6f4e41b206dc094d455efaa111a809f37efe259d
Jared <jaredlll08@gmail.com>
2018-11-19 02:00:15 +0200

    version push

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/CraftTweaker.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/CraftTweaker.java
@@ -43,7 +43,7 @@ import org.apache.logging.log4j.Logger;
  * Main mod class. Performs some general logic, initialization of the API and
  * FML event handling.
  */
-@Mod(modid = CraftTweaker.MODID, version = "4.1.11", name = CraftTweaker.NAME, acceptedMinecraftVersions = "[1.12, 1.13)")
+@Mod(modid = CraftTweaker.MODID, version = "4.1.12", name = CraftTweaker.NAME, acceptedMinecraftVersions = "[1.12, 1.13)")
 public class CraftTweaker {
     
     public static final String MODID = "crafttweaker";
