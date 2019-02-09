4c35338b837866c52862a104b35560b597b92e42
kindlich <kindlich@gmx.net>
2019-02-09 09:47:19 +0100

    Added Cleanup method to MCRecipeManager Checks that all recipes have actually been added and warns the user of recipes that have not. Also removes them from the list so that the JEI handlers won't have to check that again or add invalid recipes to JEI

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/world/expand/ExpandVector3d.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/world/expand/ExpandVector3d.java
@@ -1,9 +1,7 @@
 package crafttweaker.mc1120.world.expand;
 
 import crafttweaker.annotations.ZenRegister;
-import crafttweaker.api.util.Position3f;
 import crafttweaker.api.world.IVector3d;
-import crafttweaker.mc1120.util.MCPosition3f;
 import crafttweaker.mc1120.world.MCVector3d;
 import net.minecraft.util.math.Vec3d;
 import stanhebben.zenscript.annotations.ZenExpansion;
