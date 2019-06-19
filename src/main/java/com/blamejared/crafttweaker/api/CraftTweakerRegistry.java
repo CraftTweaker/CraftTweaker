d00fd91f1efc3a6896f41e2e55e85e6c6ff62dcf
Jared <jaredlll08@gmail.com>
2019-06-19 15:10:34 +0200

    Stopped using a label, extracted to method instead. Also fixed class reg

--- src/main/java/com/blamejared/crafttweaker/api/CraftTweakerRegistry.java
+++ src/main/java/com/blamejared/crafttweaker/api/CraftTweakerRegistry.java
@@ -2,7 +2,7 @@ package com.blamejared.crafttweaker.api;
 
 import com.blamejared.crafttweaker.CraftTweaker;
 import com.blamejared.crafttweaker.api.annotations.ZenRegister;
-import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.*;
 import net.minecraftforge.forgespi.language.ModFileScanData;
 import org.objectweb.asm.Type;
 
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 public class CraftTweakerRegistry {
     
     private static final List<Class> ZEN_CLASSES = new ArrayList<>();
-    
     private static final Type TYPE_ZEN_REGISTER = Type.getType(ZenRegister.class);
     
     /**
@@ -20,17 +19,31 @@ public class CraftTweakerRegistry {
      */
     public static void findClasses() {
         final List<ModFileScanData.AnnotationData> annotations = ModList.get().getAllScanData().stream().map(ModFileScanData::getAnnotations).flatMap(Collection::stream).filter(a -> TYPE_ZEN_REGISTER.equals(a.getAnnotationType())).collect(Collectors.toList());
-        modLoop:
         for(ModFileScanData.AnnotationData data : annotations) {
+            try {
+                addClass(data);
+            } catch(ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Adds a class to {@link CraftTweakerRegistry#ZEN_CLASSES} if it's required mod deps are found
+     *
+     * @param data Scan data for a given class.
+     */
+    private static void addClass(ModFileScanData.AnnotationData data) throws ClassNotFoundException {
+        if(data.getAnnotationData().containsKey("modDeps")) {
             List<String> modOnly = (List<String>) data.getAnnotationData().get("modDeps");
             for(String mod : modOnly) {
                 if(mod != null && !mod.isEmpty() && !ModList.get().isLoaded(mod)) {
-                    continue modLoop;
+                    return;
                 }
             }
-            CraftTweaker.LOG.info("Found ZenRegister: {}", data.getClass().getName());
-            ZEN_CLASSES.add(data.getClass());
         }
+        CraftTweaker.LOG.info("Found ZenRegister: {}", data.getClassType().getClassName());
+        ZEN_CLASSES.add(Class.forName(data.getClassType().getClassName(), false, CraftTweaker.class.getClassLoader()));
     }
     
     
