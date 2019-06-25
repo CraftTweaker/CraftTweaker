0e61f5edd2cb8ee0d77bc4b3a43b089933d6cc63
Jared <jaredlll08@gmail.com>
2019-06-26 01:07:05 +0200

    Made logging no pad

--- src/main/java/com/blamejared/crafttweaker/api/logger/LogLevel.java
+++ src/main/java/com/blamejared/crafttweaker/api/logger/LogLevel.java
@@ -1,23 +1,11 @@
 package com.blamejared.crafttweaker.api.logger;
 
-import java.util.Arrays;
-
 public enum LogLevel {
     
     DEBUG, INFO, WARNING, ERROR;
-    
-    /**
-     * The maximum character length of any enum name
-     * Used to make print formats easier
-     */
-    public static final int MAX_LENGTH = Arrays.stream(LogLevel.values()).map(Enum::name).mapToInt(String::length).max().orElse(7);
     
     public boolean canLog(LogLevel other) {
         return this.compareTo(other) <= 0;
     }
     
-    @Override
-    public String toString() {
-        return String.format("%" + MAX_LENGTH + "s", super.toString());
-    }
 }
