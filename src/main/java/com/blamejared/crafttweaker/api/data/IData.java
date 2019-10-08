0b29883eef221974e862620af8bb604aaf17b6ef
Jared <jaredlll08@gmail.com>
2019-10-09 01:02:00 +0200

    update forge, fix ingredient serializer, more docs

--- src/main/java/com/blamejared/crafttweaker/api/data/IData.java
+++ src/main/java/com/blamejared/crafttweaker/api/data/IData.java
@@ -10,12 +10,12 @@ import java.util.Map;
 
 /**
  * The IData interface is a generic Interface for handling Data like NBT.
- * You can cast about all primitives (short, double, string, int, …) as well as certain arrays to IData.
+ * You can cast about all primitives (short, double, string, int, ...) as well as certain arrays to IData.
  * Remember that while they offer similar features, IData and their counterparts are NOT the same, which is why they will be referred to as DataTypes (e.g. ByteData).
  */
 @ZenCodeType.Name("crafttweaker.api.data.IData")
 @ZenRegister
-@Document()
+@Document
 public interface IData {
     
     /**
--- src/main/java/com/blamejared/crafttweaker/impl/ingredients/IngredientNBT.java
+++ src/main/java/com/blamejared/crafttweaker/impl/ingredients/IngredientNBT.java
@@ -1,5 +1,6 @@
 package com.blamejared.crafttweaker.impl.ingredients;
 
+import com.blamejared.crafttweaker.CraftTweaker;
 import com.google.gson.JsonObject;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
@@ -10,42 +11,45 @@ import net.minecraftforge.common.crafting.IIngredientSerializer;
 import javax.annotation.Nullable;
 import java.util.stream.Stream;
 
-public class IngredientNBT extends Ingredient
-{
+public class IngredientNBT extends Ingredient {
+    
     private final ItemStack stack;
-    public IngredientNBT(ItemStack stack)
-    {
+    
+    public IngredientNBT(ItemStack stack) {
         super(Stream.of(new Ingredient.SingleItemList(stack)));
         this.stack = stack;
     }
-
+    
     @Override
-    public boolean test(@Nullable ItemStack input)
-    {
-        if (input == null)
+    public boolean test(@Nullable ItemStack input) {
+        if(input == null)
             return false;
         //Can't use areItemStacksEqualUsingNBTShareTag because it compares stack size as well
         return this.stack.getItem() == input.getItem() && this.stack.getDamage() == input.getDamage() && this.stack.areShareTagsEqual(input);
     }
-
+    
     @Override
-    public boolean isSimple()
-    {
+    public boolean isSimple() {
         return false;
     }
-
-    public static class Serializer implements IIngredientSerializer<IngredientNBT>
-    {
+    
+    @Override
+    public IIngredientSerializer<? extends Ingredient> getSerializer() {
+        return CraftTweaker.INGREDIENT_NBT_SERIALIZER;
+    }
+    
+    public static class Serializer implements IIngredientSerializer<IngredientNBT> {
+        
         @Override
         public IngredientNBT parse(PacketBuffer buffer) {
             return new IngredientNBT(buffer.readItemStack());
         }
-
+        
         @Override
         public IngredientNBT parse(JsonObject json) {
             return new IngredientNBT(CraftingHelper.getItemStack(json, true));
         }
-
+        
         @Override
         public void write(PacketBuffer buffer, IngredientNBT ingredient) {
             buffer.writeItemStack(ingredient.stack);
