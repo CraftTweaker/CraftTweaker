585249622604b98a4aec24e34b150753c64f6b60
Jared <jaredlll08@gmail.com>
2019-10-08 02:39:41 +0200

    MCEntityType Tag

--- src/main/java/com/blamejared/crafttweaker/impl/helper/CraftTweakerHelper.java
+++ src/main/java/com/blamejared/crafttweaker/impl/helper/CraftTweakerHelper.java
@@ -2,7 +2,9 @@ package com.blamejared.crafttweaker.impl.helper;
 
 import com.blamejared.crafttweaker.api.item.IItemStack;
 import com.blamejared.crafttweaker.impl.blocks.MCBlock;
+import com.blamejared.crafttweaker.impl.entity.MCEntityType;
 import net.minecraft.block.Block;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -20,6 +22,10 @@ public class CraftTweakerHelper {
     
     public static Block[] getBlocks(MCBlock[] blocks) {
         return Arrays.stream(blocks).map(MCBlock::getInternal).toArray(Block[]::new);
+    }
+    
+    public static EntityType[] getEntityTypes(MCEntityType[] entities) {
+        return Arrays.stream(entities).map(MCEntityType::getInternal).toArray(EntityType[]::new);
     }
     
 }
