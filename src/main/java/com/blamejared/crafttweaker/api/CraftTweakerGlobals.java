6a6ababd74818678c52aefdd7d01e43b619d6a94
Jared <jaredlll08@gmail.com>
2019-10-11 22:26:30 +0200

    add print alias for println

--- src/main/java/com/blamejared/crafttweaker/api/CraftTweakerGlobals.java
+++ src/main/java/com/blamejared/crafttweaker/api/CraftTweakerGlobals.java
@@ -1,7 +1,7 @@
 package com.blamejared.crafttweaker.api;
 
 import com.blamejared.crafttweaker.api.annotations.ZenRegister;
-import org.openzen.zencode.java.*;
+import org.openzen.zencode.java.ZenCodeGlobals;
 
 @ZenRegister
 public class CraftTweakerGlobals {
@@ -9,6 +9,11 @@ public class CraftTweakerGlobals {
     @ZenCodeGlobals.Global
     public static void println(String msg) {
         CraftTweakerAPI.logger.info(msg);
+    }
+    
+    @ZenCodeGlobals.Global
+    public static void print(String msg) {
+        println(msg);
     }
     
 }
