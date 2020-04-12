7d7315fba0e56d78c9c7a6e0601fac68af9e6587
Jared <jaredlll08@gmail.com>
2020-04-12 06:24:00 +0200

    Implement /ct hand tags

--- src/main/java/com/blamejared/crafttweaker/CraftTweaker.java
+++ src/main/java/com/blamejared/crafttweaker/CraftTweaker.java
@@ -114,7 +114,7 @@ public class CraftTweaker {
                 URLConnection urlConnection = url.openConnection();
                 urlConnection.setConnectTimeout(15000);
                 urlConnection.setReadTimeout(15000);
-                urlConnection.setRequestProperty("User-Agent", "CraftTweaker");
+                urlConnection.setRequestProperty("User-Agent", "CraftTweaker|1.15.2");
                 try(BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
                     PATRON_LIST = reader.lines().filter(s -> !s.isEmpty()).collect(Collectors.toSet());
                 }
