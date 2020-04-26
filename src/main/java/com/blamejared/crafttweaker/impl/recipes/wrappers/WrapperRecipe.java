717a54b8692af4aba2a3584fe719736da6d46571
Jared <jaredlll08@gmail.com>
2020-04-27 01:32:00 +0200

    add `getRecipeByName` and `getRecipesByOutput` (NYI) Close #935

---
+++ src/main/java/com/blamejared/crafttweaker/impl/recipes/wrappers/WrapperRecipe.java
@@ -0,0 +1,58 @@
+package com.blamejared.crafttweaker.impl.recipes.wrappers;
+
+import com.blamejared.crafttweaker.api.item.*;
+import com.blamejared.crafttweaker.impl.item.*;
+import net.minecraft.item.crafting.IRecipe;
+import org.openzen.zencode.java.ZenCodeType;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ZenCodeType.Name("crafttweaker.api.recipes.WrapperRecipe")
+public class WrapperRecipe {
+    
+    private final IRecipe<?> recipe;
+    
+    public WrapperRecipe(IRecipe<?> recipe) {
+        this.recipe = recipe;
+    }
+    
+    @ZenCodeType.Getter("id")
+    public String getId() {
+        return getRecipe().getId().toString();
+    }
+    
+    @ZenCodeType.Getter("group")
+    public String getGroup() {
+        return getRecipe().getGroup();
+    }
+    
+    @ZenCodeType.Getter("output")
+    public IItemStack getOutput() {
+        return new MCItemStack(getRecipe().getRecipeOutput());
+    }
+    
+    @ZenCodeType.Getter("dynamic")
+    public boolean isDynamic() {
+        return getRecipe().isDynamic();
+    }
+    
+    @ZenCodeType.Method
+    public boolean canFit(int width, int height) {
+        return getRecipe().canFit(width, height);
+    }
+    
+    @ZenCodeType.Getter("icon")
+    public IItemStack getIcon() {
+        return new MCItemStackMutable(getRecipe().getIcon());
+    }
+    
+    @ZenCodeType.Getter("ingredients")
+    public List<IIngredient> getIngredients() {
+        return getRecipe().getIngredients().stream().map(IIngredient::fromIngredient).collect(Collectors.toList());
+    }
+    
+    public IRecipe<?> getRecipe() {
+        return recipe;
+    }
+}
