0a7afcd60eaeb2d53eb1074be7af90f9595eb0cd
Jared <jaredlll08@gmail.com>
2020-07-27 13:35:37 +0200

    fix nbt matching for vanilla ingredients

--- src/main/java/com/blamejared/crafttweaker/impl/item/MCItemStackMutable.java
+++ src/main/java/com/blamejared/crafttweaker/impl/item/MCItemStackMutable.java
@@ -113,6 +113,9 @@ public class MCItemStackMutable implements IItemStack {
         if(getInternal().isEmpty()){
             return Ingredient.EMPTY;
         }
+        if(!getInternal().hasTag()){
+            return Ingredient.fromStacks(getInternal());
+        }
         return new IngredientNBT(getInternal());
     }
     
