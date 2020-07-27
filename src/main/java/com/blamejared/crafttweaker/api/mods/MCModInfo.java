6c6a5b4cd95c81a6ec62ba749b8c5fc1d71e93af
Jared <jaredlll08@gmail.com>
2020-07-27 13:36:14 +0200

    add a way to get items blocks and entitytypes from mods

--- src/main/java/com/blamejared/crafttweaker/api/mods/MCModInfo.java
+++ src/main/java/com/blamejared/crafttweaker/api/mods/MCModInfo.java
@@ -1,9 +1,19 @@
 package com.blamejared.crafttweaker.api.mods;
 
 import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+import com.blamejared.crafttweaker.api.item.IItemStack;
+import com.blamejared.crafttweaker.impl.blocks.MCBlock;
+import com.blamejared.crafttweaker.impl.entity.MCEntityType;
+import com.blamejared.crafttweaker.impl.item.MCItemStack;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.openzen.zencode.java.ZenCodeType;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ZenRegister
 @ZenCodeType.Name("crafttweaker.api.mods.ModInfo")
@@ -45,6 +55,35 @@ public class MCModInfo {
     public String getVersion() {
         return getModInfo().getVersion().toString();
     }
+    
+    @ZenCodeType.Getter("items")
+    public List<IItemStack> getItems() {
+        return ForgeRegistries.ITEMS.getEntries().stream()
+                .filter(resourceLocationItemEntry -> resourceLocationItemEntry.getKey().getNamespace().equals(getModId()))
+                .map(Map.Entry::getValue)
+                .map(ItemStack::new)
+                .map(MCItemStack::new)
+                .collect(Collectors.toList());
+    }
+    
+    @ZenCodeType.Getter("blocks")
+    public List<MCBlock> getBlocks() {
+        return ForgeRegistries.BLOCKS.getEntries().stream()
+                .filter(resourceLocationItemEntry -> resourceLocationItemEntry.getKey().getNamespace().equals(getModId()))
+                .map(Map.Entry::getValue)
+                .map(MCBlock::new)
+                .collect(Collectors.toList());
+    }
+    
+    @ZenCodeType.Getter("entitytypes")
+    public List<MCEntityType> getEntityTypes() {
+        return ForgeRegistries.ENTITIES.getEntries().stream()
+                .filter(resourceLocationItemEntry -> resourceLocationItemEntry.getKey().getNamespace().equals(getModId()))
+                .map(Map.Entry::getValue)
+                .map(MCEntityType::new)
+                .collect(Collectors.toList());
+    }
+    
     
     /**
      * Gets the namespace of the mod
