45a0ab9c4a44f2136c3697b56eb8e7b1189313ab
kindlich <kindlich@gmx.net>
2020-08-08 20:21:38 +0200

    Added JUnit tests for Snipping Preprocessor
    (cherry picked from commit f5efac5acddb47de87a510c41ad34bdf2902bb2b)

---
+++ src/test/java/com/blamejared/crafttweaker/api/TestLogger.java
@@ -0,0 +1,21 @@
+package com.blamejared.crafttweaker.api;
+
+import com.blamejared.crafttweaker.api.logger.*;
+
+public class TestLogger implements ILogger {
+    
+    @Override
+    public void setLogLevel(LogLevel logLevel) {
+        //NoOp
+    }
+    
+    @Override
+    public LogLevel getLogLevel() {
+        return LogLevel.DEBUG;
+    }
+    
+    @Override
+    public void log(LogLevel level, String message, boolean prefix) {
+        System.out.printf("[%s] %s%n", level, message);
+    }
+}
