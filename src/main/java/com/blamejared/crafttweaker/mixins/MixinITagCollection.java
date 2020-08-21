d7591c35e6adcaf8da9d903325e58ccd54c3a0b1
Jared <jaredlll08@gmail.com>
2020-08-22 00:49:46 +0200

    1.16.2 port is done (tm) mixins :eyes:
    May crash, seemed fine with my scripts.

---
+++ src/main/java/com/blamejared/crafttweaker/mixins/MixinITagCollection.java
@@ -0,0 +1,26 @@
+package com.blamejared.crafttweaker.mixins;
+
+import com.google.common.collect.*;
+import net.minecraft.tags.*;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.*;
+
+import java.util.Map;
+
+@Mixin(targets = "net.minecraft.tags.ITagCollection$1")
+public abstract class MixinITagCollection<T> {
+    
+    @Shadow(aliases = "val$bimap")
+    private BiMap<ResourceLocation, ITag<T>> bimap;
+    
+    @Inject(method = "func_241833_a", at = @At("RETURN"), cancellable = true)
+    private void func_241833_a(CallbackInfoReturnable<Map<ResourceLocation, ITag<T>>> cir) {
+        if(bimap instanceof ImmutableBiMap) {
+            bimap = HashBiMap.create(bimap);
+        }
+        cir.setReturnValue(bimap);
+    }
+}
