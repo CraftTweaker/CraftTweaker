03fc98f8654dbc774232361f9621f3dc347e763e
Colin Wong <cwongmath@gmail.com>
2020-09-20 00:58:43 -0500

    Make default

--- CraftTweaker2-API/src/main/java/crafttweaker/api/player/IPlayer.java
+++ CraftTweaker2-API/src/main/java/crafttweaker/api/player/IPlayer.java
@@ -1,5 +1,6 @@
 package crafttweaker.api.player;
 
+import crafttweaker.CraftTweakerAPI;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.chat.IChatMessage;
 import crafttweaker.api.data.IData;
@@ -119,9 +120,15 @@ public interface IPlayer extends IEntityLivingBase, IUser {
     void setCanEdit(boolean canEdit);
     
     @ZenMethod
-    IEntityItem dropItem(boolean dropAll);
+    default IEntityItem dropItem(boolean dropAll) {
+        CraftTweakerAPI.logError("Default method IPlayer#dropItem(bool) is not overwritten in " + getClass() + " please report to the author!");
+        return null;
+    }
     
     @ZenMethod
-    IEntityItem dropItem(IItemStack item);
+    default IEntityItem dropItem(IItemStack item) {
+        CraftTweakerAPI.logError("Default method IPlayer#dropItem(IItemStack) is not overwritten in " + getClass() + " please report to the author!");
+        return null;
+    }
     
 }
