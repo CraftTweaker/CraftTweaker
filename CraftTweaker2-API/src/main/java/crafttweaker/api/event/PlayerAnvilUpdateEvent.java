083517c9fc85cc18c36299b5094a4e805feb949f
Colin Wong <cwongmath@gmail.com>
2020-09-20 12:55:23 -0500

    Add AnvilUpdateEvent to api

---
+++ CraftTweaker2-API/src/main/java/crafttweaker/api/event/PlayerAnvilUpdateEvent.java
@@ -0,0 +1,36 @@
+package crafttweaker.api.event;
+
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenGetter;
+import stanhebben.zenscript.annotations.ZenSetter;
+
+@ZenClass("crafttweaker.event.PlayerAnvilUpdateEvent")
+@ZenRegister
+public interface PlayerAnvilUpdateEvent extends IEventCancelable, IHasCancellationResult {
+    
+    @ZenGetter("leftItem")
+    IItemStack getLeftItem();
+    
+    @ZenGetter("rightItem")
+    IItemStack getRightItem();
+    
+    @ZenGetter("outputItem")
+    IItemStack getOutputItem();
+    
+    @ZenSetter("outputItem")
+    void setOutputItem(IItemStack output);
+    
+    @ZenGetter("xpCost")
+    int getXPCost();
+    
+    @ZenSetter("xpCost")
+    void setXPCost(int xp);
+    
+    @ZenGetter("materialCost")
+    int getMaterialCost();
+    
+    @ZenSetter("materialCost")
+    void setMaterialCost(int materialCost);
+}
