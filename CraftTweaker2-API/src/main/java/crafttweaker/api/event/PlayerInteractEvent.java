8c910fa54f37c6e516d601068dea27a9f114f96d
Colin Wong <cwongmath@gmail.com>
2020-11-04 20:03:19 -0600

    Add face getter to API

--- CraftTweaker2-API/src/main/java/crafttweaker/api/event/PlayerInteractEvent.java
+++ CraftTweaker2-API/src/main/java/crafttweaker/api/event/PlayerInteractEvent.java
@@ -1,9 +1,11 @@
 package crafttweaker.api.event;
 
+import crafttweaker.CraftTweakerAPI;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.block.IBlock;
 import crafttweaker.api.block.IBlockState;
 import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.world.IFacing;
 import crafttweaker.api.world.IWorld;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenGetter;
@@ -37,4 +39,10 @@ public interface PlayerInteractEvent extends IPlayerEvent, IEventPositionable {
 
     @ZenGetter("item")
     IItemStack getUsedItem();
+    
+    @ZenGetter("face")
+    default IFacing getFace() {
+        CraftTweakerAPI.logError("Default method PlayerInteractEvent#getFace is not overwritten in " + getClass() + " please report to the author!");
+        return null;
+    };
 }
