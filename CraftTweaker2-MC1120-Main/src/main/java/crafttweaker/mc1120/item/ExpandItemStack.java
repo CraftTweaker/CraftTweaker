83ffad4553ff2798bf87145f767292fdd742a1a2
youyihj <30400991+friendlyhj@users.noreply.github.com>
2021-02-12 02:28:49 +0800

    add IItemStack capNBT access, close #1114 (#1161)

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/item/ExpandItemStack.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/item/ExpandItemStack.java
@@ -1,12 +1,15 @@
 package crafttweaker.mc1120.item;
 
 import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.data.IData;
 import crafttweaker.api.entity.IEntityEquipmentSlot;
 import crafttweaker.api.entity.attribute.IEntityAttributeModifier;
 import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.mc1120.data.NBTConverter;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import stanhebben.zenscript.annotations.ZenExpansion;
 import stanhebben.zenscript.annotations.ZenGetter;
 import stanhebben.zenscript.annotations.ZenMethod;
@@ -33,5 +36,21 @@ public class ExpandItemStack {
     @ZenMethod
     public static int getMaxItemUseDuration(IItemStack stack) {
         return getInternal(stack).getMaxItemUseDuration();
+    }
+
+    @ZenGetter("capNBT")
+    @ZenMethod
+    public static IData getCapNBT(IItemStack stack) {
+        return CraftTweakerMC.getIData(CraftTweakerMC.getItemStack(stack).serializeNBT().getCompoundTag("ForgeCaps"));
+    }
+
+    @ZenMethod
+    public static IItemStack withCapNBT(IItemStack stack, IData capNBT) {
+        return CraftTweakerMC.getIItemStack(new ItemStack(
+                CraftTweakerMC.getItem(stack.getDefinition()),
+                stack.getAmount(),
+                stack.getMetadata(),
+                (NBTTagCompound) NBTConverter.from(capNBT))
+        ).withTag(stack.getTag(), stack.getMatchTagExact());
     }
 }
