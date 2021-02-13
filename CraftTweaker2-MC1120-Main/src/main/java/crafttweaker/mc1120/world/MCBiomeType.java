066f4b797eb99bb973b1ca80d896d306575c104d
youyihj <youyi580@qq.com>
2021-02-13 22:02:09 +0800

    fix: IBiomeType is not registered and missing biomes getter (see docs)

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/world/MCBiomeType.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/world/MCBiomeType.java
@@ -1,7 +1,11 @@
 package crafttweaker.mc1120.world;
 
+import crafttweaker.api.world.IBiome;
 import crafttweaker.api.world.IBiomeType;
 import net.minecraftforge.common.BiomeDictionary;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MCBiomeType implements IBiomeType {
 
@@ -18,5 +22,10 @@ public class MCBiomeType implements IBiomeType {
     @Override
     public String getName() {
         return type.getName();
+    }
+
+    @Override
+    public List<IBiome> getBiomes() {
+        return BiomeDictionary.getBiomes(type).stream().map(MCBiome::new).collect(Collectors.toList());
     }
 }
