13d03cc443e104b67b93f233e57f92961f6ab17b
youyihj <30400991+friendlyhj@users.noreply.github.com>
2021-02-14 05:02:48 +0800

    fix: IBiomeType is not registered and missing biomes getter (see docs) (#1164)

--- CraftTweaker2-API/src/main/java/crafttweaker/api/world/IBiomeType.java
+++ CraftTweaker2-API/src/main/java/crafttweaker/api/world/IBiomeType.java
@@ -1,7 +1,11 @@
 package crafttweaker.api.world;
 
+import crafttweaker.annotations.ZenRegister;
 import stanhebben.zenscript.annotations.*;
 
+import java.util.List;
+
+@ZenRegister
 @ZenClass("crafttweaker.world.IBiomeType")
 public interface IBiomeType {
 
@@ -9,5 +13,7 @@ public interface IBiomeType {
     
     @ZenGetter("name")
     String getName();
-    
+
+    @ZenGetter("biomes")
+    List<IBiome> getBiomes();
 }
