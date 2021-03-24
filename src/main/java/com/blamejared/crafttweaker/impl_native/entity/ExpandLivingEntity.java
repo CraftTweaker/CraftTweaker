4653907cb3f8292a82d59a1e8d4852a2ed6f0549
youyihj <30400991+friendlyhj@users.noreply.github.com>
2021-03-25 07:42:27 +0800

    Update src/main/java/com/blamejared/crafttweaker/impl_native/entity/ExpandLivingEntity.java
    Co-authored-by: Jared <jaredlll08@gmail.com>

--- src/main/java/com/blamejared/crafttweaker/impl_native/entity/ExpandLivingEntity.java
+++ src/main/java/com/blamejared/crafttweaker/impl_native/entity/ExpandLivingEntity.java
@@ -262,7 +262,7 @@ public class ExpandLivingEntity {
     }
     
     /**
-     * Gets the item stack of the entity in given slot.
+     * Gets the ItemStack in the specific slot.
      *
      * @return The item stack of the entity in given slot.
      */
