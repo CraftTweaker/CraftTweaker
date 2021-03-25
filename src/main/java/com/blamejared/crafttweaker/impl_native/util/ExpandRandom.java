28fe1d5829c4dd26f0d33d33101907edcd69d9d4
youyihj <youyi580@qq.com>
2021-03-25 09:24:09 +0800

    MathHelper stuff

--- src/main/java/com/blamejared/crafttweaker/impl_native/util/ExpandRandom.java
+++ src/main/java/com/blamejared/crafttweaker/impl_native/util/ExpandRandom.java
@@ -3,6 +3,7 @@ package com.blamejared.crafttweaker.impl_native.util;
 import com.blamejared.crafttweaker.api.annotations.ZenRegister;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
 import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraft.util.math.MathHelper;
 import org.openzen.zencode.java.ZenCodeType;
 
 import java.util.Random;
@@ -49,9 +50,37 @@ public class ExpandRandom {
      * Returns the next pseudorandom, uniformly distributed float value
      * between 0.0f and 1.0f from this random number generator's sequence.
      */
-
     @ZenCodeType.Method
     public static float nextFloat(Random internal) {
         return internal.nextFloat();
+    }
+
+    /**
+     * Returns the next pseudorandom int. Its range is [min, max]
+     */
+    @ZenCodeType.Method
+    public static int nextInt(Random internal, int min, int max) {
+        return MathHelper.nextInt(internal, min, max);
+    }
+
+    /**
+     * Returns the next pseudorandom float. Its range is [min, max]
+     */
+    @ZenCodeType.Method
+    public static float nextFloat(Random internal, float min, float max) {
+        return MathHelper.nextFloat(internal, min, max);
+    }
+
+    /**
+     * Returns the next pseudorandom double. Its range is [min, max]
+     */
+    @ZenCodeType.Method
+    public static double nextDouble(Random internal, double min, double max) {
+        return MathHelper.nextDouble(internal, min, max);
+    }
+
+    @ZenCodeType.Method
+    public static String getRandomUUID(Random internal) {
+        return MathHelper.getRandomUUID(internal).toString();
     }
 }
