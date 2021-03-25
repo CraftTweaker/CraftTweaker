d174b2feccaca2b86271c0698cac4a8101a5dd33
youyihj <youyi580@qq.com>
2021-03-25 08:15:39 +0800

    wait, I forgot to add ZenMethod

--- CraftTweaker2-API/src/main/java/crafttweaker/api/util/IRandom.java
+++ CraftTweaker2-API/src/main/java/crafttweaker/api/util/IRandom.java
@@ -2,17 +2,23 @@ package crafttweaker.api.util;
 
 import crafttweaker.annotations.ZenRegister;
 import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
 
 @ZenClass("crafttweaker.util.IRandom")
 @ZenRegister
 public interface IRandom {
+    @ZenMethod
     int nextInt();
 
+    @ZenMethod
     int nextInt(int bound);
 
+    @ZenMethod
     double nextDouble();
 
+    @ZenMethod
     float nextFloat();
 
+    @ZenMethod
     boolean nextBoolean();
 }
