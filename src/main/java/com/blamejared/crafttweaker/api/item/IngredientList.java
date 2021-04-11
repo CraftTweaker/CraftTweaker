a02dbf2eb815f2e785c20596de37f0747ae116a0
Sara Freimer <richard@freimer.com>
2021-04-10 20:56:38 -0400

    Remove unneeded isSimple override (#1235)

--- src/main/java/com/blamejared/crafttweaker/api/item/IngredientList.java
+++ src/main/java/com/blamejared/crafttweaker/api/item/IngredientList.java
@@ -41,12 +41,6 @@ public class IngredientList extends CompoundIngredient {
         return Serializer.INSTANCE;
     }
     
-    @Override
-    public boolean isSimple() {
-        
-        return false;
-    }
-    
     public static class Serializer implements IIngredientSerializer<IngredientList> {
         
         public static final Serializer INSTANCE = new Serializer();
