3c642abfbd80cd2e7739e4aac89dd90a6d6787e3
TheSilkMiner <thesilkminer@outlook.com>
2021-04-23 12:20:39 +0200

    Create helper for usage of method handles
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/java/com/blamejared/crafttweaker/api/util/MethodHandleHelper.java
@@ -0,0 +1,98 @@
+package com.blamejared.crafttweaker.api.util;
+
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.WrongMethodTypeException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public final class MethodHandleHelper {
+    public static final class UnableToLinkMethodHandleException extends RuntimeException {
+        private UnableToLinkMethodHandleException(final String message, final Throwable cause) {
+            super(message, cause);
+        }
+    }
+    
+    public static final class FailedInvocationException extends RuntimeException {
+        private FailedInvocationException(final String message, final Throwable cause)  {
+            super(message, cause);
+        }
+    }
+    
+    public interface MethodHandleInvoker<R> {
+        R invoke() throws Throwable;
+    }
+    
+    public interface MethodHandleVoidInvoker {
+        void invoke() throws Throwable;
+    }
+    
+    private MethodHandleHelper() {}
+    
+    public static MethodHandle link(final Class<?> type, final String methodName, final Class<?>... arguments) {
+        try {
+            final MethodHandles.Lookup lookup = MethodHandles.lookup();
+            final Method target = ObfuscationReflectionHelper.findMethod(type, methodName, arguments);
+            
+            return lookup.unreflect(target);
+        } catch (final ObfuscationReflectionHelper.UnableToFindMethodException e) {
+            throw new UnableToLinkMethodHandleException("Method " + StringUtils.quoteAndEscape(methodName) + " was not found inside class " + type.getName(), e);
+        } catch (final IllegalAccessException e) {
+            throw new UnableToLinkMethodHandleException("Unable to access method " + StringUtils.quoteAndEscape(methodName), e);
+        }
+    }
+    
+    public static <T> MethodHandle linkGetter(final Class<? super T> type, final String fieldName) {
+        try {
+            final MethodHandles.Lookup lookup = MethodHandles.lookup();
+            final Field target = ObfuscationReflectionHelper.findField(type, fieldName);
+        
+            return lookup.unreflectGetter(target);
+        } catch (final ObfuscationReflectionHelper.UnableToFindFieldException e) {
+            throw new UnableToLinkMethodHandleException("Field " + StringUtils.quoteAndEscape(fieldName) + " was not found inside class " + type.getName(), e);
+        } catch (final IllegalAccessException e) {
+            throw new UnableToLinkMethodHandleException("Unable to access field " + StringUtils.quoteAndEscape(fieldName), e);
+        }
+    }
+    
+    public static <T> MethodHandle linkSetter(final Class<? super T> type, final String fieldName) {
+        try {
+            final MethodHandles.Lookup lookup = MethodHandles.lookup();
+            final Field target = ObfuscationReflectionHelper.findField(type, fieldName);
+            
+            return lookup.unreflectSetter(target);
+        } catch (final ObfuscationReflectionHelper.UnableToFindFieldException e) {
+            throw new UnableToLinkMethodHandleException("Field " + StringUtils.quoteAndEscape(fieldName) + " was not found inside class " + type.getName(), e);
+        } catch (final IllegalAccessException e) {
+            throw new UnableToLinkMethodHandleException("Unable to access field " + StringUtils.quoteAndEscape(fieldName), e);
+        }
+    }
+    
+    public static <R> R invoke(final MethodHandleInvoker<R> invoker) {
+        try {
+            return invoker.invoke();
+        } catch (final WrongMethodTypeException e) {
+            throw new FailedInvocationException("Unable to invoke target method handle: check your arguments", e);
+        } catch (final Throwable throwable) {
+            if (throwable instanceof RuntimeException) {
+                throw (RuntimeException) throwable;
+            }
+            throw new RuntimeException("Wrapping", throwable);
+        }
+    }
+    
+    public static void invokeVoid(final MethodHandleVoidInvoker invoker) {
+        try {
+            invoker.invoke();
+        } catch (final WrongMethodTypeException e) {
+            throw new FailedInvocationException("Unable to invoke target method handle: check your arguments", e);
+        } catch (final Throwable throwable) {
+            if (throwable instanceof RuntimeException) {
+                throw (RuntimeException) throwable;
+            }
+            throw new RuntimeException("Wrapping", throwable);
+        }
+    }
+}
