a075cdac18b27e8aec1a1d5a4d5f1cb08640efcd
TheSilkMiner <thesilkminer@outlook.com>
2021-04-28 21:10:28 +0200

    Add cooking and stonecutter replacement handlers
    These are still early on and feature repeated logic: IRecipeHandler may
    provide a similar method as a static for ease of usage, if this is the
    direction that we want to follow. This is merely to show off a potential
    implementation.
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/blamejared/crafttweaker/impl/recipes/handlers/vanilla/CookingRecipeHandler.java
+++ src/main/java/com/blamejared/crafttweaker/impl/recipes/handlers/vanilla/CookingRecipeHandler.java
@@ -3,6 +3,7 @@ package com.blamejared.crafttweaker.impl.recipes.handlers.vanilla;
 import com.blamejared.crafttweaker.api.item.IIngredient;
 import com.blamejared.crafttweaker.api.managers.IRecipeManager;
 import com.blamejared.crafttweaker.api.recipes.IRecipeHandler;
+import com.blamejared.crafttweaker.api.recipes.ReplacementRule;
 import com.blamejared.crafttweaker.api.util.StringUtils;
 import com.blamejared.crafttweaker.impl.item.MCItemStackMutable;
 import com.google.common.collect.ImmutableMap;
@@ -17,7 +18,9 @@ import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.crafting.SmokingRecipe;
 import net.minecraft.util.ResourceLocation;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @IRecipeHandler.For(BlastingRecipe.class)
 @IRecipeHandler.For(CampfireCookingRecipe.class)
@@ -50,4 +53,25 @@ public final class CookingRecipeHandler implements IRecipeHandler<AbstractCookin
                 recipe.getCookTime()
         );
     }
+    
+    @Override
+    public Optional<AbstractCookingRecipe> replaceIngredients(final IRecipeManager manager, final AbstractCookingRecipe recipe, final List<ReplacementRule> rules) {
+        
+        final Ingredient originalInput = recipe.getIngredients().get(0);
+        /*mutable*/ Ingredient input = originalInput;
+        for (final ReplacementRule rule : rules) {
+            if (rule.shouldReplace(input)) {
+                input = rule.getVanillaTo();
+            }
+        }
+        
+        if (originalInput == input) return Optional.empty();
+        
+        final AbstractCookingRecipe newRecipe = LOOKUP.get(recipe.getType())
+                .getSecond()
+                .create(recipe.getId(), recipe.getGroup(), input, recipe.getRecipeOutput(), recipe.getExperience(), recipe.getCookTime());
+        
+        return Optional.of(newRecipe);
+    }
+    
 }
--- src/main/java/com/blamejared/crafttweaker/impl/recipes/handlers/vanilla/StoneCutterRecipeHandler.java
+++ src/main/java/com/blamejared/crafttweaker/impl/recipes/handlers/vanilla/StoneCutterRecipeHandler.java
@@ -3,9 +3,14 @@ package com.blamejared.crafttweaker.impl.recipes.handlers.vanilla;
 import com.blamejared.crafttweaker.api.item.IIngredient;
 import com.blamejared.crafttweaker.api.managers.IRecipeManager;
 import com.blamejared.crafttweaker.api.recipes.IRecipeHandler;
+import com.blamejared.crafttweaker.api.recipes.ReplacementRule;
 import com.blamejared.crafttweaker.api.util.StringUtils;
 import com.blamejared.crafttweaker.impl.item.MCItemStackMutable;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.crafting.StonecuttingRecipe;
+
+import java.util.List;
+import java.util.Optional;
 
 @IRecipeHandler.For(StonecuttingRecipe.class)
 public final class StoneCutterRecipeHandler implements IRecipeHandler<StonecuttingRecipe> {
@@ -19,6 +24,22 @@ public final class StoneCutterRecipeHandler implements IRecipeHandler<Stonecutti
                 new MCItemStackMutable(recipe.getRecipeOutput()).getCommandString(),
                 IIngredient.fromIngredient(recipe.getIngredients().get(0)).getCommandString()
         );
+    }
+    
+    @Override
+    public Optional<StonecuttingRecipe> replaceIngredients(final IRecipeManager manager, final StonecuttingRecipe recipe, final List<ReplacementRule> rules) {
+        
+        final Ingredient originalInput = recipe.getIngredients().get(0);
+        /*mutable*/ Ingredient input = originalInput;
+        for (final ReplacementRule rule : rules) {
+            if (rule.shouldReplace(input)) {
+                input = rule.getVanillaTo();
+            }
+        }
+    
+        if (originalInput == input) return Optional.empty();
+
+        return Optional.of(new StonecuttingRecipe(recipe.getId(), recipe.getGroup(), input, recipe.getRecipeOutput()));
     }
     
 }
