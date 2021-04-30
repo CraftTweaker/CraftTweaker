d309e3d6448f5094bc627071ce086622fba8446c
Tom Dodd <joedodd35@gmail.com>
2021-04-30 23:38:29 +0100

    Revert "Revert "Minor modification ScriptFile to make toString use non-capitalized loader names""
    This reverts commit 3af12b2f7516ecd9ab975004988983902025445d.

--- CraftTweaker2-API/src/main/java/crafttweaker/runtime/ScriptFile.java
+++ CraftTweaker2-API/src/main/java/crafttweaker/runtime/ScriptFile.java
@@ -179,9 +179,20 @@ public class ScriptFile {
     }
     
     /**
+     * Compliant with the lower-case names in the original toString method
+     */
+    private String loaderNamesToString() {
+        StringBuilder builder = new StringBuilder();
+        for (String name : getLoaderNames()) {
+            builder.append('|').append(name);
+        }
+        return builder.substring(1);
+    }
+    
+    /**
      * Compliant with the capitalization used in the name class generator
      */
-    public String loaderNamesToString() {
+    public String loaderNamesConcatCapitalized() {
         StringBuilder builder = new StringBuilder();
         for (String name : getLoaderNames()) {
             builder.append(capitalize(name));
