c6550b23a538ae91b253f2c8546fa12c588e4c82
Jared <jaredlll08@gmail.com>
2021-05-03 03:33:56 +0200

    fix material color crashing on servers
    getMapColor is clientonly

--- src/main/java/com/blamejared/crafttweaker/impl_native/block/material/ExpandMaterialColor.java
+++ src/main/java/com/blamejared/crafttweaker/impl_native/block/material/ExpandMaterialColor.java
@@ -11,9 +11,38 @@ import org.openzen.zencode.java.ZenCodeType;
 @NativeTypeRegistration(value = MaterialColor.class, zenCodeName = "crafttweaker.api.block.material.MCMaterialColor")
 public class ExpandMaterialColor {
     
+    /**
+     * Gets the Integer value of the colour of this material color on a map.
+     *
+     * @param index the index to get the colour for.
+     *
+     * @return the Integer value of the map colour
+     *
+     * @docParam index 1
+     */
     @ZenCodeType.Method
     public static int getMapColor(MaterialColor internal, int index) {
-        return internal.getMapColor(index);
+        //TODO 1.17 change this to call the MaterialColor method when it isn't client only.
+        int i;
+        switch(index) {
+            case 0:
+                i = 180;
+                break;
+            default:
+                i = 220;
+                break;
+            case 2:
+                i = 255;
+                break;
+            case 3:
+                i = 135;
+                break;
+        }
+        
+        int j = (internal.colorValue >> 16 & 255) * i / 255;
+        int k = (internal.colorValue >> 8 & 255) * i / 255;
+        int l = (internal.colorValue & 255) * i / 255;
+        return -16777216 | l << 16 | k << 8 | j;
     }
     
     
