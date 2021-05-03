8f419c6ef1f55333a0ca5f994c4deef356cab3aa
TheSilkMiner <thesilkminer@outlook.com>
2021-05-03 18:20:33 +0200

    Add missed replacement for smithing recipes
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/blamejared/crafttweaker/impl/recipes/handlers/vanilla/SmithingRecipeHandler.java
+++ src/main/java/com/blamejared/crafttweaker/impl/recipes/handlers/vanilla/SmithingRecipeHandler.java
@@ -3,9 +3,14 @@ package com.blamejared.crafttweaker.impl.recipes.handlers.vanilla;
 import com.blamejared.crafttweaker.api.item.IIngredient;
 import com.blamejared.crafttweaker.api.managers.IRecipeManager;
 import com.blamejared.crafttweaker.api.recipes.IRecipeHandler;
+import com.blamejared.crafttweaker.api.recipes.IReplacementRule;
 import com.blamejared.crafttweaker.api.util.StringUtils;
 import com.blamejared.crafttweaker.impl.item.MCItemStackMutable;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.crafting.SmithingRecipe;
+
+import java.util.List;
+import java.util.Optional;
 
 @IRecipeHandler.For(SmithingRecipe.class)
 public final class SmithingRecipeHandler implements IRecipeHandler<SmithingRecipe> {
@@ -20,6 +25,16 @@ public final class SmithingRecipeHandler implements IRecipeHandler<SmithingRecip
                 IIngredient.fromIngredient(recipe.base).getCommandString(),
                 IIngredient.fromIngredient(recipe.addition).getCommandString()
         );
+    }
+    
+    @Override
+    public Optional<SmithingRecipe> replaceIngredients(final IRecipeManager manager, final SmithingRecipe recipe, final List<IReplacementRule> rules) {
+        final Optional<Ingredient> base = IRecipeHandler.attemptReplacing(recipe.base, Ingredient.class, rules);
+        final Optional<Ingredient> addition = IRecipeHandler.attemptReplacing(recipe.addition, Ingredient.class, rules);
+        
+        if (!base.isPresent() && !addition.isPresent()) return Optional.empty();
+        
+        return Optional.of(new SmithingRecipe(recipe.getId(), base.orElseGet(() -> recipe.base), addition.orElseGet(() -> recipe.addition), recipe.getRecipeOutput()));
     }
     
 }
