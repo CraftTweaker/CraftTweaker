652ef01fa6a42ee86950b5415efa6709f6c1db25
youyihj <youyi580@qq.com>
2021-06-02 15:34:33 +0800

    tab -> 4 spaces

--- CraftTweaker2-MC1120-Mod-JEI/src/main/java/crafttweaker/mods/jei/actions/DescribeAction.java
+++ CraftTweaker2-MC1120-Mod-JEI/src/main/java/crafttweaker/mods/jei/actions/DescribeAction.java
@@ -14,47 +14,47 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DescribeAction implements IAction {
-	private final List<IItemStack> itemStacks = new ArrayList<>();
-	private final List<ILiquidStack> fluidStacks = new ArrayList<>();
-	private final String[] description;
-	private final String name;
-	
-	@Deprecated
-	public DescribeAction(IItemStack stack, String[] description) {
-		this(Collections.singletonList(stack), description, stack.toString());
-	}
-	/**
-	 * Used to add a JEI Description page to the given ingredients
-	 * @param ingredients The ingredients to be given descriptions
-	 * @param description The Description: one or more lines per item, auto wrap if lines are too long
-	 * @param name Solely for the User output, should be a representation of stack 
-	 */
-	public DescribeAction(List<? extends IIngredient> ingredients, String[] description, String name) {
-		ingredients.forEach(it -> {
-			List<ILiquidStack> liquids = it.getLiquids();
-			if (liquids.isEmpty()) {
-				itemStacks.addAll(it.getItems());
-			} else {
-				fluidStacks.addAll(liquids);
-			}
-		});
-		this.description = description;
-		this.name = name;
-	}
+    private final List<IItemStack> itemStacks = new ArrayList<>();
+    private final List<ILiquidStack> fluidStacks = new ArrayList<>();
+    private final String[] description;
+    private final String name;
 
-	@Override
-	public void apply() {
-		if (!itemStacks.isEmpty()) {
-			JEIAddonPlugin.modRegistry.addIngredientInfo(itemStacks.stream().map(CraftTweakerMC::getItemStack).collect(Collectors.toList()), VanillaTypes.ITEM, description);
-		}
+    @Deprecated
+    public DescribeAction(IItemStack stack, String[] description) {
+        this(Collections.singletonList(stack), description, stack.toString());
+    }
+    /**
+     * Used to add a JEI Description page to the given ingredients
+     * @param ingredients The ingredients to be given descriptions
+     * @param description The Description: one or more lines per item, auto wrap if lines are too long
+     * @param name Solely for the User output, should be a representation of stack
+     */
+    public DescribeAction(List<? extends IIngredient> ingredients, String[] description, String name) {
+        ingredients.forEach(it -> {
+            List<ILiquidStack> liquids = it.getLiquids();
+            if (liquids.isEmpty()) {
+                itemStacks.addAll(it.getItems());
+            } else {
+                fluidStacks.addAll(liquids);
+            }
+        });
+        this.description = description;
+        this.name = name;
+    }
 
-		if (!fluidStacks.isEmpty()) {
-			JEIAddonPlugin.modRegistry.addIngredientInfo(fluidStacks.stream().map(CraftTweakerMC::getLiquidStack).collect(Collectors.toList()), VanillaTypes.FLUID, description);
-		}
-	}
+    @Override
+    public void apply() {
+        if (!itemStacks.isEmpty()) {
+            JEIAddonPlugin.modRegistry.addIngredientInfo(itemStacks.stream().map(CraftTweakerMC::getItemStack).collect(Collectors.toList()), VanillaTypes.ITEM, description);
+        }
 
-	@Override
-	public String describe() {
-		return "Adding description in JEI for: " + name;
-	}
+        if (!fluidStacks.isEmpty()) {
+            JEIAddonPlugin.modRegistry.addIngredientInfo(fluidStacks.stream().map(CraftTweakerMC::getLiquidStack).collect(Collectors.toList()), VanillaTypes.FLUID, description);
+        }
+    }
+
+    @Override
+    public String describe() {
+        return "Adding description in JEI for: " + name;
+    }
 }
