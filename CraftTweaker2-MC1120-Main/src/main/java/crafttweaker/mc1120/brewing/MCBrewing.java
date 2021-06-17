b297b4ae60ca0143ac1345011879183c64ce15de
youyihj <youyi580@qq.com>
2021-06-17 23:53:45 +0800

    tab -> 4 spaces

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/brewing/MCBrewing.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/brewing/MCBrewing.java
@@ -15,37 +15,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MCBrewing implements IBrewingManager{
-	private static final List<Tuple<IIngredient, IIngredient>> toRemoveVanillaRecipes = new ArrayList<>();
-	private final List<IBrewingRecipe> allBrewingRecipes = CraftTweakerHacks.getPrivateStaticObject(BrewingRecipeRegistry.class, "recipes");
-	
-	public MCBrewing() {
-	}
+    private static final List<Tuple<IIngredient, IIngredient>> toRemoveVanillaRecipes = new ArrayList<>();
+    private final List<IBrewingRecipe> allBrewingRecipes = CraftTweakerHacks.getPrivateStaticObject(BrewingRecipeRegistry.class, "recipes");
 
-	@Override
-	public void addBrew(IIngredient input, IIngredient ingredient, IItemStack output, boolean hidden) {
-		CraftTweakerAPI.apply(new ActionAddBrewingRecipe(input, new IIngredient[] {ingredient}, output, hidden));		
-	}
+    public MCBrewing() {
+    }
 
-	@Override
-	public void addBrew(IIngredient input, IIngredient[] ingredients, IItemStack output, boolean hidden) {
-		CraftTweakerAPI.apply(new ActionAddBrewingRecipe(input, ingredients, output, hidden));
-	}
-	
-	@Override
-	public void removeRecipe(IItemStack input, IItemStack ingredient) {
-		if (toRemoveVanillaRecipes.isEmpty()) { // toRemoveVanillaRecipes list is empty means the first time to call the method
-			CraftTweaker.LATE_ACTIONS.add(new ActionFixVanillaBrewingRecipes(toRemoveVanillaRecipes, allBrewingRecipes));
-		}
-		toRemoveVanillaRecipes.add(new Tuple<>(input, ingredient));
-		CraftTweaker.LATE_ACTIONS.add(new ActionRemoveBrewingRecipe(input, ingredient, allBrewingRecipes));
-	}
+    @Override
+    public void addBrew(IIngredient input, IIngredient ingredient, IItemStack output, boolean hidden) {
+        CraftTweakerAPI.apply(new ActionAddBrewingRecipe(input, new IIngredient[] {ingredient}, output, hidden));
+    }
 
-	@Override
-	public void removeRecipe(IItemStack ingredient) {
-		if (toRemoveVanillaRecipes.isEmpty()) {
-			CraftTweaker.LATE_ACTIONS.add(new ActionFixVanillaBrewingRecipes(toRemoveVanillaRecipes, allBrewingRecipes));
-		}
-		toRemoveVanillaRecipes.add(new Tuple<>(IngredientAny.INSTANCE, ingredient));
-		CraftTweaker.LATE_ACTIONS.add(new ActionRemoveBrewingRecipeForIngredient(ingredient, allBrewingRecipes));
-	}
+    @Override
+    public void addBrew(IIngredient input, IIngredient[] ingredients, IItemStack output, boolean hidden) {
+        CraftTweakerAPI.apply(new ActionAddBrewingRecipe(input, ingredients, output, hidden));
+    }
+
+    @Override
+    public void removeRecipe(IItemStack input, IItemStack ingredient) {
+        if (toRemoveVanillaRecipes.isEmpty()) { // toRemoveVanillaRecipes list is empty means the first time to call the method
+            CraftTweaker.LATE_ACTIONS.add(new ActionFixVanillaBrewingRecipes(toRemoveVanillaRecipes, allBrewingRecipes));
+        }
+        toRemoveVanillaRecipes.add(new Tuple<>(input, ingredient));
+        CraftTweaker.LATE_ACTIONS.add(new ActionRemoveBrewingRecipe(input, ingredient, allBrewingRecipes));
+    }
+
+    @Override
+    public void removeRecipe(IItemStack ingredient) {
+        if (toRemoveVanillaRecipes.isEmpty()) {
+            CraftTweaker.LATE_ACTIONS.add(new ActionFixVanillaBrewingRecipes(toRemoveVanillaRecipes, allBrewingRecipes));
+        }
+        toRemoveVanillaRecipes.add(new Tuple<>(IngredientAny.INSTANCE, ingredient));
+        CraftTweaker.LATE_ACTIONS.add(new ActionRemoveBrewingRecipeForIngredient(ingredient, allBrewingRecipes));
+    }
 }
