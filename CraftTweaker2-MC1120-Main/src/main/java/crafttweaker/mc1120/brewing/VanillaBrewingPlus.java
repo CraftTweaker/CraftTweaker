05ba7dffa7b0abe87ce0b35252cc3228cf7cba56
youyihj <youyi580@qq.com>
2021-06-18 09:17:49 +0800

    brewing.remove(<*>, <item>); -> remove the item from valid ingredients

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/brewing/VanillaBrewingPlus.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/brewing/VanillaBrewingPlus.java
@@ -2,38 +2,47 @@ package crafttweaker.mc1120.brewing;
 
 import crafttweaker.api.item.IIngredient;
 import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.item.IngredientAny;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Tuple;
 import net.minecraftforge.common.brewing.VanillaBrewingRecipe;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public final class VanillaBrewingPlus extends VanillaBrewingRecipe {
-	
-	private final List<Tuple<IIngredient, IIngredient>> removedRecipes;
 
-	public VanillaBrewingPlus(List<Tuple<IIngredient, IIngredient>> removedRecipes) {
-		this.removedRecipes = removedRecipes;
-	}
-	
-	@Override
-	public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
-		IItemStack _input = CraftTweakerMC.getIItemStack(input);
-		IItemStack _ingredient = CraftTweakerMC.getIItemStack(ingredient);
-		
-		if (removedRecipes.stream().anyMatch(t -> t.getFirst().matches(_input) && t.getSecond().matches(_ingredient))) {
-			return ItemStack.EMPTY;
-		}
-		
-		return super.getOutput(input, ingredient);
-	}
-	
-	public ItemStack getRealOutput(ItemStack input, ItemStack ingredient) {
-		return super.getOutput(input, ingredient);
-	}
-	
-	public List<Tuple<IIngredient, IIngredient>> getRemovedRecipes() {
-		return removedRecipes;
-	}
+    private final List<Tuple<IIngredient, IIngredient>> removedRecipes;
+
+    public VanillaBrewingPlus(List<Tuple<IIngredient, IIngredient>> removedRecipes) {
+        this.removedRecipes = removedRecipes;
+    }
+
+    @Override
+    public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+        IItemStack _input = CraftTweakerMC.getIItemStack(input);
+        IItemStack _ingredient = CraftTweakerMC.getIItemStack(ingredient);
+
+        if (removedRecipes.stream().anyMatch(t -> t.getFirst().matches(_input) && t.getSecond().matches(_ingredient))) {
+            return ItemStack.EMPTY;
+        }
+
+        return super.getOutput(input, ingredient);
+    }
+
+    @Override
+    public boolean isIngredient(@Nonnull ItemStack stack) {
+        IItemStack _ingredient = CraftTweakerMC.getIItemStack(stack);
+
+        return super.isIngredient(stack) && removedRecipes.stream().noneMatch(t -> t.getFirst() == IngredientAny.INSTANCE && t.getSecond().matches(_ingredient));
+    }
+
+    public ItemStack getRealOutput(ItemStack input, ItemStack ingredient) {
+        return super.getOutput(input, ingredient);
+    }
+
+    public List<Tuple<IIngredient, IIngredient>> getRemovedRecipes() {
+        return removedRecipes;
+    }
 }
