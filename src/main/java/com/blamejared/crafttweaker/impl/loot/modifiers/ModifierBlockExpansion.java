601332e685d501c611da8d9a316d8745730282ea
TheSilkMiner <thesilkminer@outlook.com>
2021-06-21 20:49:14 +0200

    Add ToolType support in Loot Modifier extensions
    TODO: Handle entities because of equipment predicate not supporting tool
    types. Maybe an expansion?
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/blamejared/crafttweaker/impl/loot/modifiers/ModifierBlockExpansion.java
+++ src/main/java/com/blamejared/crafttweaker/impl/loot/modifiers/ModifierBlockExpansion.java
@@ -5,11 +5,13 @@ import com.blamejared.crafttweaker.api.item.IItemStack;
 import com.blamejared.crafttweaker.api.loot.modifiers.ILootModifier;
 import com.blamejared.crafttweaker.impl.loot.CTLootManager;
 import com.blamejared.crafttweaker.impl.loot.conditions.CTLootConditionBuilder;
+import com.blamejared.crafttweaker.impl.loot.conditions.crafttweaker.ToolTypeLootConditionTypeBuilder;
 import com.blamejared.crafttweaker.impl.loot.conditions.vanilla.BlockStatePropertyLootConditionTypeBuilder;
 import com.blamejared.crafttweaker.impl.loot.conditions.vanilla.MatchToolLootConditionBuilder;
 import com.blamejared.crafttweaker.impl.predicate.StatePropertiesPredicate;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
 import net.minecraft.block.Block;
+import net.minecraftforge.common.ToolType;
 import org.openzen.zencode.java.ZenCodeType;
 
 import java.util.function.Consumer;
@@ -174,6 +176,27 @@ public final class ModifierBlockExpansion {
                 CTLootConditionBuilder.create()
                         .add(BlockStatePropertyLootConditionTypeBuilder.class, bs -> bs.withBlock(internal))
                         .add(MatchToolLootConditionBuilder.class, mt -> mt.withPredicate(item -> item.matching(tool, matchDamage, false, matchNbt))),
+                modifier
+        );
+    }
+    
+    /**
+     * Adds an {@link ILootModifier} that fires if this block gets broken with a tool with the given {@link ToolType}.
+     *
+     * <p>Damage or NBT is ignored when attempting to match the tool.</p>
+     *
+     * @param internal The block to add the loot modifier to.
+     * @param name The name of the loot modifier.
+     * @param toolType The type of the tool the block must be broken with.
+     * @param modifier The loot modifier to add to the block.
+     */
+    @ZenCodeType.Method
+    public static void addToolTypeLootModifier(final Block internal, final String name, final ToolType toolType, final ILootModifier modifier) {
+        CTLootManager.LOOT_MANAGER.getModifierManager().register(
+                name,
+                CTLootConditionBuilder.create()
+                        .add(BlockStatePropertyLootConditionTypeBuilder.class, bs -> bs.withBlock(internal))
+                        .add(ToolTypeLootConditionTypeBuilder.class, tt -> tt.withToolType(toolType)),
                 modifier
         );
     }
