4c7653ec09367d32865f8545ccef363782ae299a
youyihj <youyi580@qq.com>
2021-06-28 11:37:14 +0800

    enchantment

--- src/main/java/com/blamejared/crafttweaker/api/item/IItemStack.java
+++ src/main/java/com/blamejared/crafttweaker/api/item/IItemStack.java
@@ -16,6 +16,8 @@ import com.blamejared.crafttweaker.impl.food.MCFood;
 import com.blamejared.crafttweaker.impl.item.MCWeightedItemStack;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
 import com.blamejared.crafttweaker_annotations.annotations.ZenWrapper;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.inventory.EquipmentSlotType;
@@ -635,6 +637,19 @@ public interface IItemStack extends IIngredient, IIngredientWithAmount {
     default ToolType[] getToolTypes() {
         
         return getInternal().getToolTypes().toArray(new ToolType[0]);
+    }
+
+    @ZenCodeType.Getter("enchantments")
+    default Map<Enchantment, Integer> getEnchantments() {
+        return EnchantmentHelper.getEnchantments(getInternal());
+    }
+
+    /**
+     * Gets the level of given enchantment on the item. Returns 0 if the item doesn't have given enchantment.
+     */
+    @ZenCodeType.Method
+    default int getEnchantmentLevel(Enchantment enchantment) {
+        return EnchantmentHelper.getEnchantmentLevel(enchantment, getInternal());
     }
     
     /**
