b125ef2b9593ff07d88e1813b351a677dbe4c3b3
kindlich <kindlich@gmx.net>
2021-06-30 00:33:31 +0200

    NativeMethod Annotation, first steps (#1293)

--- src/main/java/com/blamejared/crafttweaker/impl/native_types/NativeTypeRegistry.java
+++ src/main/java/com/blamejared/crafttweaker/impl/native_types/NativeTypeRegistry.java
@@ -1,11 +1,10 @@
 package com.blamejared.crafttweaker.impl.native_types;
 
-import com.blamejared.crafttweaker.api.CraftTweakerAPI;
-import com.blamejared.crafttweaker_annotations.annotations.NativeConstructor;
-import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import com.blamejared.crafttweaker.api.*;
+import com.blamejared.crafttweaker_annotations.annotations.*;
 
+import java.lang.reflect.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Holds a listing of all registered vanilla types.
@@ -16,46 +15,86 @@ public class NativeTypeRegistry {
     private final Map<Class<?>, CrTNativeTypeInfo> nativeTypeInfos = new HashMap<>();
     
     private static Class<?>[] convertConstructorToClassArray(NativeConstructor nativeConstructor) {
+        
         return Arrays.stream(nativeConstructor.value())
                 .map(NativeConstructor.ConstructorParameter::type)
                 .toArray(Class<?>[]::new);
     }
     
-    public void addNativeType(NativeTypeRegistration registration) {
+    public void addNativeType(NativeTypeRegistration registration, NativeMethod[] nativeMethods) {
+        
         final Class<?> vanillaClass = registration.value();
         final String crtName = registration.zenCodeName();
-        final List<Class<?>[]> constructors = getNativeConstructors(registration);
+        final Map<String, CrTNativeExecutableRefs> executables = getNativeExecutables(registration, nativeMethods);
+        
         
         if(nativeTypeInfos.containsKey(vanillaClass) && !nativeTypeInfos.get(vanillaClass)
                 .getCraftTweakerName()
                 .equals(crtName)) {
             final String format = "Trying to register vanilla class '%s' twice with different names: '%s' and '%s'";
             final CrTNativeTypeInfo existingNativeTypeInfo = nativeTypeInfos.get(vanillaClass);
-            CraftTweakerAPI.logError(format, existingNativeTypeInfo.getCraftTweakerName(), crtName);
+            CraftTweakerAPI.logError(format, vanillaClass.getCanonicalName(), existingNativeTypeInfo.getCraftTweakerName(), crtName);
         } else {
-            nativeTypeInfos.put(vanillaClass, new CrTNativeTypeInfo(vanillaClass, crtName, constructors));
+            nativeTypeInfos.put(vanillaClass, new CrTNativeTypeInfo(vanillaClass, crtName, executables));
         }
     }
     
-    private List<Class<?>[]> getNativeConstructors(NativeTypeRegistration registration) {
-        return Arrays.stream(registration.constructors())
-                .map(NativeTypeRegistry::convertConstructorToClassArray)
-                .collect(Collectors.toList());
+    private Map<String, CrTNativeExecutableRefs> getNativeExecutables(NativeTypeRegistration registration, NativeMethod[] nativeMethods) {
+        
+        final HashMap<String, CrTNativeExecutableRefs> result = new HashMap<>();
+        
+        for(NativeConstructor constructor : registration.constructors()) {
+            final Class<?>[] classes = convertConstructorToClassArray(constructor);
+            result.computeIfAbsent("<init>", i -> new CrTNativeExecutableRefs())
+                    .createForSignature(classes)
+                    .withConstructorAnnotation();
+        }
+        
+        for(NativeMethod nativeMethod : nativeMethods) {
+            result.computeIfAbsent(nativeMethod.name(), i -> new CrTNativeExecutableRefs())
+                    .createForSignature(nativeMethod.parameters())
+                    .withGetter(nativeMethod.getterName())
+                    .withSetter(nativeMethod.setterName())
+                    .withMethodAnnotation();
+        }
+        
+        return result;
     }
     
     public boolean hasInfoFor(Class<?> clazz) {
+        
         return nativeTypeInfos.containsKey(clazz);
     }
     
     public String getCrTNameFor(Class<?> clazz) {
+        
         return getTypeInfoFor(clazz).getCraftTweakerName();
     }
     
     public Collection<CrTNativeTypeInfo> getNativeTypeInfos() {
+        
         return nativeTypeInfos.values();
     }
     
     public CrTNativeTypeInfo getTypeInfoFor(Class<?> clazz) {
+        
         return nativeTypeInfos.get(clazz);
     }
+    
+    public Optional<CrTNativeExecutableRef> getMethodInfoFor(Constructor<?> constructor) {
+        
+        if(hasInfoFor(constructor.getDeclaringClass())) {
+            return getTypeInfoFor(constructor.getDeclaringClass()).getMethod(constructor);
+        }
+        return Optional.empty();
+    }
+    
+    public Optional<CrTNativeExecutableRef> getMethodInfoFor(Method method) {
+        
+        if(hasInfoFor(method.getDeclaringClass())) {
+            return getTypeInfoFor(method.getDeclaringClass()).getMethod(method);
+        }
+        return Optional.empty();
+    }
+    
 }
---
+++ src/main/java/com/blamejared/crafttweaker/impl_native/util/ExpandForgeRegistryEntry.java
@@ -0,0 +1,10 @@
+package com.blamejared.crafttweaker.impl_native.util;
+
+import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraftforge.registries.ForgeRegistryEntry;
+
+@ZenRegister
+@NativeTypeRegistration(value = ForgeRegistryEntry.class, zenCodeName = "crafttweaker.api.util.ForgeRegistryEntry")
+public class ExpandForgeRegistryEntry {
+}
