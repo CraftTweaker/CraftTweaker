20084ebb7c9aa3058e7454912e425e2232703633
Jared <jaredlll08@gmail.com>
2021-08-11 23:10:39 +0200

    fix getEnchantmentLevel not returning levels for enchanted books

--- src/main/java/com/blamejared/crafttweaker/api/item/IItemStack.java
+++ src/main/java/com/blamejared/crafttweaker/api/item/IItemStack.java
@@ -669,7 +669,7 @@ public interface IItemStack extends IIngredient, IIngredientWithAmount {
     @ZenCodeType.Method
     default int getEnchantmentLevel(Enchantment enchantment) {
         
-        return EnchantmentHelper.getEnchantmentLevel(enchantment, getInternal());
+        return getEnchantments().getOrDefault(enchantment, 0);
     }
     
     /**
