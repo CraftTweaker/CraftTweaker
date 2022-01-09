0fec90642b2e8d42380f7cfffdd537e3349c20e0
Jared <jaredlll08@gmail.com>
2022-01-09 23:40:58 +0200

    re-add forge registryName getters

---
+++ Forge/src/main/java/com/blamejared/crafttweaker/natives/util/ExpandForgeRegistryEntry.java
@@ -0,0 +1,10 @@
+package com.blamejared.crafttweaker.natives.util;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraftforge.registries.ForgeRegistryEntry;
+
+@ZenRegister
+@NativeTypeRegistration(value = ForgeRegistryEntry.class, zenCodeName = "crafttweaker.api.util.ForgeRegistryEntry")
+public class ExpandForgeRegistryEntry {
+}
