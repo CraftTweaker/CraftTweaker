eee3e63a59829315926a36570a5ef6618bfc3a31
Jared <jaredlll08@gmail.com>
2022-01-10 14:21:53 +0200

    Re-add stone cutter support

---
+++ Common/src/main/java/com/blamejared/crafttweaker/api/recipe/manager/StoneCutterManager.java
@@ -0,0 +1,51 @@
+package com.blamejared.crafttweaker.api.recipe.manager;
+
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
+import com.blamejared.crafttweaker.api.CraftTweakerConstants;
+import com.blamejared.crafttweaker.api.action.recipe.ActionAddRecipe;
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.ingredient.IIngredient;
+import com.blamejared.crafttweaker.api.item.IItemStack;
+import com.blamejared.crafttweaker.api.recipe.manager.base.IRecipeManager;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.StonecutterRecipe;
+import org.openzen.zencode.java.ZenCodeGlobals;
+import org.openzen.zencode.java.ZenCodeType;
+
+/**
+ * @docParam this stoneCutter
+ */
+@ZenRegister
+@ZenCodeType.Name("crafttweaker.api.recipe.StoneCutterManager")
+@Document("vanilla/api/recipe/manager/StoneCutterManager")
+public class StoneCutterManager implements IRecipeManager<StonecutterRecipe> {
+    
+    @ZenCodeGlobals.Global("stoneCutter")
+    public static final StoneCutterManager INSTANCE = new StoneCutterManager();
+    
+    /**
+     * Adds a recipe to the stone cutter
+     *
+     * @param recipeName name of the recipe
+     * @param output     output {@link IItemStack}
+     * @param input      input {@link IIngredient}
+     *
+     * @docParam recipeName "recipe_name"
+     * @docParam output <item:minecraft:grass>
+     * @docParam input <tag:items:minecraft:wool>
+     */
+    @ZenCodeType.Method
+    public void addRecipe(String recipeName, IItemStack output, IIngredient input) {
+        
+        recipeName = fixRecipeName(recipeName);
+        CraftTweakerAPI.apply(new ActionAddRecipe<>(this, new StonecutterRecipe(CraftTweakerConstants.rl(recipeName), "", input.asVanillaIngredient(), output.getInternal()), ""));
+    }
+    
+    @Override
+    public RecipeType<StonecutterRecipe> getRecipeType() {
+        
+        return RecipeType.STONECUTTING;
+    }
+    
+}
---
+++ Common/src/main/java/com/blamejared/crafttweaker/natives/recipe/type/ExpandStonecutterRecipe.java
@@ -0,0 +1,13 @@
+package com.blamejared.crafttweaker.natives.recipe.type;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraft.world.item.crafting.StonecutterRecipe;
+
+@ZenRegister
+@Document("vanilla/api/recipe/type/type/StonecutterRecipe")
+@NativeTypeRegistration(value = StonecutterRecipe.class, zenCodeName = "crafttweaker.api.recipe.type.StonecutterRecipe")
+public class ExpandStonecutterRecipe {
+
+}
