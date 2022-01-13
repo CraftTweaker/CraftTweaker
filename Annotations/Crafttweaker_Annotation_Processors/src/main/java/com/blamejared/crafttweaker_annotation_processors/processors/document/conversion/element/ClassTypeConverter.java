fbabee3757929effb202e3ae17c17ca295cddded
Jared <jaredlll08@gmail.com>
2022-01-13 03:22:37 +0200

    handle null types better for docs generation

--- Annotations/Crafttweaker_Annotation_Processors/src/main/java/com/blamejared/crafttweaker_annotation_processors/processors/document/conversion/element/ClassTypeConverter.java
+++ Annotations/Crafttweaker_Annotation_Processors/src/main/java/com/blamejared/crafttweaker_annotation_processors/processors/document/conversion/element/ClassTypeConverter.java
@@ -4,16 +4,19 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.MirroredTypeException;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 import java.lang.annotation.Annotation;
 import java.util.function.Function;
 
 public class ClassTypeConverter {
     
     private final Elements elementUtils;
+    private final Types types;
     
-    public ClassTypeConverter(Elements elementUtils) {
+    public ClassTypeConverter(Elements elementUtils, Types types) {
         
         this.elementUtils = elementUtils;
+        this.types = types;
     }
     
     public <T extends Annotation> TypeMirror getTypeMirror(T annotation, Function<T, Class<?>> classGetter) {
@@ -31,6 +34,9 @@ public class ClassTypeConverter {
         
         final String typeName = exception.typeName().replace('$', '.');
         final TypeElement typeElement = elementUtils.getTypeElement(typeName);
+        if(typeElement == null){
+            return types.getNullType();
+        }
         return typeElement.asType();
     }
     
