a075710ac5d4d5dc70d557b200b61afb860c16e3
Jared <jaredlll08@gmail.com>
2022-01-30 22:49:38 +0200

    Expose IItemStack methods correctly

--- Fabric/src/main/java/com/blamejared/crafttweaker/api/item/FabricItemStack.java
+++ Fabric/src/main/java/com/blamejared/crafttweaker/api/item/FabricItemStack.java
@@ -1,13 +1,12 @@
 package com.blamejared.crafttweaker.api.item;
 
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
-import com.blamejared.crafttweaker_annotations.annotations.Document;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import org.openzen.zencode.java.ZenCodeType;
 
 @ZenRegister
 @ZenCodeType.Name("crafttweaker.api.item.FabricItemStack")
-@Document("fabric/api/item/FabricItemStack")
+//@Document("fabric/api/item/FabricItemStack")
 public interface FabricItemStack extends IItemStack {
     
     @Override
--- Forge/src/main/java/com/blamejared/crafttweaker/api/item/ForgeItemStack.java
+++ Forge/src/main/java/com/blamejared/crafttweaker/api/item/ForgeItemStack.java
@@ -1,14 +1,13 @@
 package com.blamejared.crafttweaker.api.item;
 
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
-import com.blamejared.crafttweaker_annotations.annotations.Document;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.ToolAction;
 import org.openzen.zencode.java.ZenCodeType;
 
 @ZenRegister
 @ZenCodeType.Name("crafttweaker.api.item.ForgeItemStack")
-@Document("forge/api/item/ForgeItemStack")
+//@Document("forge/api/item/ForgeItemStack")
 public interface ForgeItemStack extends IItemStack {
     
     @Override
@@ -23,8 +22,10 @@ public interface ForgeItemStack extends IItemStack {
      * @param action The action to perform.
      *
      * @return True if it can perform the action, false otherwise.
+     *
+     * @deprecated Method has been moved to an expansion, so it exists in IItemStack for scripters, and ExpandIItemStackForge for modders.
      */
-    @ZenCodeType.Method
+    @Deprecated(forRemoval = true)
     default boolean canPerformAction(ToolAction action) {
         
         return getInternal().canPerformAction(action);
