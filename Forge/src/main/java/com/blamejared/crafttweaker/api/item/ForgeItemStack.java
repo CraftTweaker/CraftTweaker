a711693e8a77d238e0eca299a353e1b9ed844dce
Jared <jaredlll08@gmail.com>
2022-01-30 08:00:14 +0200

    Expose canPerformAction

--- Forge/src/main/java/com/blamejared/crafttweaker/api/item/ForgeItemStack.java
+++ Forge/src/main/java/com/blamejared/crafttweaker/api/item/ForgeItemStack.java
@@ -3,6 +3,7 @@ package com.blamejared.crafttweaker.api.item;
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
 import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.common.ToolAction;
 import org.openzen.zencode.java.ZenCodeType;
 
 @ZenRegister
@@ -14,6 +15,19 @@ public interface ForgeItemStack extends IItemStack {
     default int getBurnTime() {
         
         return ForgeHooks.getBurnTime(getInternal(), null);
+    }
+    
+    /**
+     * Checks if this item can perform the given ToolAction.
+     *
+     * @param action The action to perform.
+     *
+     * @return True if it can perform the action, false otherwise.
+     */
+    @ZenCodeType.Method
+    default boolean canPerformAction(ToolAction action) {
+        
+        return getInternal().canPerformAction(action);
     }
     
 }
