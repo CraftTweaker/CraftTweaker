0f43dd62d4ec2258d7e307a2cde9954eb585a5d5
Jared <jaredlll08@gmail.com>
2022-01-30 23:16:37 +0200

    Fix build

--- Fabric/src/main/java/com/blamejared/crafttweaker/api/item/FabricItemStack.java
+++ Fabric/src/main/java/com/blamejared/crafttweaker/api/item/FabricItemStack.java
@@ -1,12 +1,13 @@
 package com.blamejared.crafttweaker.api.item;
 
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import org.openzen.zencode.java.ZenCodeType;
 
 @ZenRegister
 @ZenCodeType.Name("crafttweaker.api.item.FabricItemStack")
-//@Document("fabric/api/item/FabricItemStack")
+@Document("fabric/api/item/FabricItemStack")
 public interface FabricItemStack extends IItemStack {
     
     @Override
--- Forge/src/main/java/com/blamejared/crafttweaker/api/item/ForgeItemStack.java
+++ Forge/src/main/java/com/blamejared/crafttweaker/api/item/ForgeItemStack.java
@@ -1,13 +1,14 @@
 package com.blamejared.crafttweaker.api.item;
 
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.ToolAction;
 import org.openzen.zencode.java.ZenCodeType;
 
 @ZenRegister
 @ZenCodeType.Name("crafttweaker.api.item.ForgeItemStack")
-//@Document("forge/api/item/ForgeItemStack")
+@Document("forge/api/item/ForgeItemStack")
 public interface ForgeItemStack extends IItemStack {
     
     @Override
