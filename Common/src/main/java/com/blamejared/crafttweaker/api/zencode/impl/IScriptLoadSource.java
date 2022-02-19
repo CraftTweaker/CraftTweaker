cfde92631d65b95b565aa8cf34398dc26ec877d8
TheSilkMiner <thesilkminer@outlook.com>
2022-02-19 12:09:16 +0100

    Add helper like in IScriptLoader
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- Common/src/main/java/com/blamejared/crafttweaker/api/zencode/impl/IScriptLoadSource.java
+++ Common/src/main/java/com/blamejared/crafttweaker/api/zencode/impl/IScriptLoadSource.java
@@ -1,9 +1,15 @@
 package com.blamejared.crafttweaker.api.zencode.impl;
 
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import net.minecraft.resources.ResourceLocation;
 
 @FunctionalInterface
 public interface IScriptLoadSource {
+    
+    static IScriptLoadSource find(final ResourceLocation id) {
+        
+        return CraftTweakerAPI.getRegistry().findLoadSource(id);
+    }
     
     ResourceLocation id();
     
