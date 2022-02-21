37104540124c8a93751d0a5504b53c63971c5f41
Jared <jaredlll08@gmail.com>
2022-02-21 14:32:15 +0200

    Specify Network Direction. Close #1449

--- src/main/java/com/blamejared/crafttweaker/impl/network/PacketHandler.java
+++ src/main/java/com/blamejared/crafttweaker/impl/network/PacketHandler.java
@@ -5,10 +5,12 @@ import com.blamejared.crafttweaker.impl.network.messages.MessageCopy;
 import com.blamejared.crafttweaker.impl.network.messages.MessageOpen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.network.NetworkDirection;
 import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public class PacketHandler {
@@ -19,10 +21,10 @@ public class PacketHandler {
     
     public static void init() {
         
-        CHANNEL.registerMessage(ID++, MessageCopy.class, (messageCopy, packetBuffer) -> packetBuffer.writeString(messageCopy.toCopy), packetBuffer -> new MessageCopy(packetBuffer.readString()), (messageCopy, contextSupplier) -> andHandling(contextSupplier, () -> Minecraft.getInstance().keyboardListener.setClipboardString(messageCopy.toCopy)));
+        CHANNEL.registerMessage(ID++, MessageCopy.class, (messageCopy, packetBuffer) -> packetBuffer.writeString(messageCopy.toCopy), packetBuffer -> new MessageCopy(packetBuffer.readString()), (messageCopy, contextSupplier) -> andHandling(contextSupplier, () -> Minecraft.getInstance().keyboardListener.setClipboardString(messageCopy.toCopy)), Optional.of(NetworkDirection.PLAY_TO_CLIENT));
         CHANNEL.registerMessage(ID++, MessageOpen.class, (messageOpen, packetBuffer) -> packetBuffer.writeString(messageOpen.path), packetBuffer -> new MessageOpen(packetBuffer.readString()), (messageOpen, contextSupplier) -> andHandling(contextSupplier, () -> {
             System.out.println(String.format("Could not open %s!", messageOpen.path));
-        }));
+        }), Optional.of(NetworkDirection.PLAY_TO_CLIENT));
     }
     
     private static void andHandling(final Supplier<NetworkEvent.Context> contextSupplier, final Runnable enqueuedWork) {
