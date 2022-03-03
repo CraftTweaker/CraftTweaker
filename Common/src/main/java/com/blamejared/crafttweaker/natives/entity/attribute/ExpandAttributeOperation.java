73e563f3326ecda1d405328dcbaa94a6130a114c
Jared <jaredlll08@gmail.com>
2022-03-03 03:16:52 +0200

    Add an enum bracket to attribute operation

--- Common/src/main/java/com/blamejared/crafttweaker/natives/entity/attribute/ExpandAttributeOperation.java
+++ Common/src/main/java/com/blamejared/crafttweaker/natives/entity/attribute/ExpandAttributeOperation.java
@@ -1,6 +1,7 @@
 package com.blamejared.crafttweaker.natives.entity.attribute;
 
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.BracketEnum;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
 import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -9,6 +10,7 @@ import org.openzen.zencode.java.ZenCodeType;
 @ZenRegister
 @Document("vanilla/api/entity/attribute/AttributeOperation")
 @NativeTypeRegistration(value = AttributeModifier.Operation.class, zenCodeName = "crafttweaker.api.entity.attribute.AttributeOperation")
+@BracketEnum("minecraft:attribute/operation")
 public class ExpandAttributeOperation {
     
     /**
