01cd83a6391a8febd7e27575e43e2c012e886737
TheSilkMiner <thesilkminer@outlook.com>
2022-03-06 14:22:23 +0100

    List all inherited loaders
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- Common/src/main/java/com/blamejared/crafttweaker/impl/plugin/core/LoaderData.java
+++ Common/src/main/java/com/blamejared/crafttweaker/impl/plugin/core/LoaderData.java
@@ -2,14 +2,27 @@ package com.blamejared.crafttweaker.impl.plugin.core;
 
 import com.blamejared.crafttweaker.api.zencode.IScriptLoader;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Queue;
 
 record LoaderData(String name, Collection<IScriptLoader> inheritedLoaders) implements IScriptLoader {
     
     @Override
     public String toString() {
         
-        return "%s <- %s".formatted(this.name(), this.inheritedLoaders().stream().map(IScriptLoader::name).toList());
+        final Collection<String> flattened = new ArrayList<>();
+        final Queue<IScriptLoader> workingQueue = new ArrayDeque<>();
+        this.inheritedLoaders().forEach(workingQueue::offer);
+        
+        while(!workingQueue.isEmpty()) {
+            final IScriptLoader loader = workingQueue.poll();
+            flattened.add(loader.name());
+            loader.inheritedLoaders().forEach(workingQueue::offer);
+        }
+        
+        return "%s <- %s".formatted(this.name(), flattened);
     }
     
 }
