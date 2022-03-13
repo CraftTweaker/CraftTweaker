410156138b44ac9f273251ce873fdefeb61830e4
Jared <jaredlll08@gmail.com>
2022-03-13 22:49:59 +0200

    Remove open packet

--- Common/src/main/java/com/blamejared/crafttweaker/platform/services/INetworkHelper.java
+++ Common/src/main/java/com/blamejared/crafttweaker/platform/services/INetworkHelper.java
@@ -2,13 +2,11 @@ package com.blamejared.crafttweaker.platform.services;
 
 import com.blamejared.crafttweaker.api.CraftTweakerConstants;
 import com.blamejared.crafttweaker.impl.network.message.MessageCopy;
-import com.blamejared.crafttweaker.impl.network.message.MessageOpen;
 import net.minecraft.server.level.ServerPlayer;
 
 public interface INetworkHelper {
     
     void sendCopyMessage(ServerPlayer target, MessageCopy message);
-    void sendOpenMessage(ServerPlayer target, MessageOpen message);
     
     default String getNetworkVersion(){
         return CraftTweakerConstants.NETWORK_VERSION;
--- Forge/src/main/java/com/blamejared/crafttweaker/platform/network/ForgeNetworkHelper.java
+++ Forge/src/main/java/com/blamejared/crafttweaker/platform/network/ForgeNetworkHelper.java
@@ -2,7 +2,6 @@ package com.blamejared.crafttweaker.platform.network;
 
 import com.blamejared.crafttweaker.impl.network.PacketHandler;
 import com.blamejared.crafttweaker.impl.network.message.MessageCopy;
-import com.blamejared.crafttweaker.impl.network.message.MessageOpen;
 import com.blamejared.crafttweaker.platform.services.INetworkHelper;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.PacketDistributor;
@@ -11,12 +10,6 @@ public class ForgeNetworkHelper implements INetworkHelper {
     
     @Override
     public void sendCopyMessage(ServerPlayer target, MessageCopy message) {
-        
-        PacketHandler.CHANNEL.send(PacketDistributor.PLAYER.with(() -> target), message);
-    }
-    
-    @Override
-    public void sendOpenMessage(ServerPlayer target, MessageOpen message) {
         
         PacketHandler.CHANNEL.send(PacketDistributor.PLAYER.with(() -> target), message);
     }
