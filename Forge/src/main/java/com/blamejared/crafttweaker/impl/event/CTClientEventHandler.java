6526616b0d4c502703bcc60eccc88614be05e713
Jared <jaredlll08@gmail.com>
2022-03-31 21:40:26 +0200

    Stop setting the tag state twice and error if an invalid state occurs

--- Forge/src/main/java/com/blamejared/crafttweaker/impl/event/CTClientEventHandler.java
+++ Forge/src/main/java/com/blamejared/crafttweaker/impl/event/CTClientEventHandler.java
@@ -1,12 +1,10 @@
 package com.blamejared.crafttweaker.impl.event;
 
-import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.crafttweaker.api.CraftTweakerConstants;
 import com.blamejared.crafttweaker.impl.script.RecipeManagerScriptLoader;
 import com.blamejared.crafttweaker.platform.Services;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RecipesUpdatedEvent;
-import net.minecraftforge.event.TagsUpdatedEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,13 +17,6 @@ public class CTClientEventHandler {
     
         RecipeManagerScriptLoader.updateState(RecipeManagerScriptLoader.UpdatedState.RECIPES, event::getRecipeManager);
     }
-    
-    @SubscribeEvent
-    public static void onTagsUpdated(TagsUpdatedEvent event) {
-    
-        CraftTweakerAPI.getAccessibleElementsProvider().client().registryAccess(event.getTagManager());
-        RecipeManagerScriptLoader.updateState(RecipeManagerScriptLoader.UpdatedState.TAGS, null);
-    } 
     
     @SubscribeEvent
     public static void handleTooltips(ItemTooltipEvent e) {
