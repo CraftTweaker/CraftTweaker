ccb5b7d2b1ea2fcac59a666b9b66fe0776a327c2
Jared <jaredlll08@gmail.com>
2022-04-23 00:32:31 +0200

    Fix pair methods not being callable

--- Common/src/main/java/com/blamejared/crafttweaker/natives/util/ExpandPair.java
+++ Common/src/main/java/com/blamejared/crafttweaker/natives/util/ExpandPair.java
@@ -2,6 +2,7 @@ package com.blamejared.crafttweaker.natives.util;
 
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
+import com.blamejared.crafttweaker_annotations.annotations.NativeMethod;
 import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
 import com.mojang.datafixers.util.Pair;
 import org.openzen.zencode.java.ZenCodeType;
@@ -11,42 +12,15 @@ import java.util.function.Function;
 @ZenRegister
 @Document("vanilla/api/util/Pair")
 @NativeTypeRegistration(value = Pair.class, zenCodeName = "crafttweaker.api.util.Pair")
+@NativeMethod(name = "getFirst", getterName = "first", parameters = {})
+@NativeMethod(name = "getSecond", getterName = "second", parameters = {})
+@NativeMethod(name = "swap", parameters = {})
 public class ExpandPair {
     
     @ZenCodeType.StaticExpansionMethod
-    public static <F, S> Pair<F, S> of(F first, S second) {
+    public static <F, S> Pair<F, S> of(Class<F> fClass, Class<S> sClass, F first, S second) {
         
         return Pair.of(first, second);
-    }
-    
-    @ZenCodeType.Method
-    public static <F, S> F getFirst(Pair internal) {
-        
-        return (F) internal.getFirst();
-    }
-    
-    @ZenCodeType.Method
-    public static <F, S> S getSecond(Pair internal) {
-        
-        return (S) internal.getSecond();
-    }
-    
-    @ZenCodeType.Method
-    public static <F, S> Pair<S, F> swap(Pair internal) {
-        
-        return internal.swap();
-    }
-    
-    @ZenCodeType.Method
-    public static <F, S, F2> Pair<F2, S> mapFirst(Pair internal, Function<F, F2> function) {
-        
-        return internal.mapFirst(function);
-    }
-    
-    @ZenCodeType.Method
-    public static <F, S, S2> Pair<F, S2> mapSecond(Pair internal, Function<S, S2> function) {
-        
-        return internal.mapSecond(function);
     }
     
 }
