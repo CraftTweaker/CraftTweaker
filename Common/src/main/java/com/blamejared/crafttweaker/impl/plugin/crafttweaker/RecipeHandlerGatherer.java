6b3652fb1ca5ec94c24af408733407ab963e78d1
TheSilkMiner <thesilkminer@outlook.com>
2022-09-05 12:42:03 +0200

    Add additional checks for recipe handler
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- Common/src/main/java/com/blamejared/crafttweaker/impl/plugin/crafttweaker/RecipeHandlerGatherer.java
+++ Common/src/main/java/com/blamejared/crafttweaker/impl/plugin/crafttweaker/RecipeHandlerGatherer.java
@@ -1,7 +1,9 @@
 package com.blamejared.crafttweaker.impl.plugin.crafttweaker;
 
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.crafttweaker.api.plugin.IRecipeHandlerRegistrationHandler;
 import com.blamejared.crafttweaker.api.recipe.handler.IRecipeHandler;
+import com.blamejared.crafttweaker.api.recipe.manager.base.IRecipeManager;
 import com.blamejared.crafttweaker.api.util.ClassUtil;
 import com.blamejared.crafttweaker.api.util.GenericUtil;
 import com.blamejared.crafttweaker.api.util.InstantiationUtil;
@@ -16,12 +18,12 @@ final class RecipeHandlerGatherer {
     
     void gatherAndRegisterHandlers(final IRecipeHandlerRegistrationHandler handler) {
         
-        Stream.concat(this.findWithAnnotation(IRecipeHandler.For.class), this.findWithAnnotation(IRecipeHandler.For.Container.class))
+        Stream.concat(this.find(IRecipeHandler.For.class), this.find(IRecipeHandler.For.Container.class))
                 .distinct()
                 .forEach(it -> this.tryRegister(it, handler));
     }
     
-    private Stream<? extends Class<?>> findWithAnnotation(final Class<? extends Annotation> annotation) {
+    private Stream<? extends Class<?>> find(final Class<? extends Annotation> annotation) {
         
         return ClassUtil.findClassesWithAnnotation(annotation);
     }
@@ -37,12 +39,22 @@ final class RecipeHandlerGatherer {
         if(Modifier.isAbstract(clazz.getModifiers())) {
             throw new IllegalArgumentException("Class " + clazz.getName() + " is an abstract class and cannot be annotated with @IRecipeHandler.For");
         }
+        if(IRecipeManager.class.isAssignableFrom(clazz)) {
+            CraftTweakerAPI.LOGGER.warn("Recipe manager " + clazz.getName() + " doubles as a recipe handler: this is discouraged");
+        }
         Arrays.stream(clazz.getAnnotationsByType(IRecipeHandler.For.class))
                 .map(IRecipeHandler.For::value)
-                .map(it -> (Class<?>) it)
+                .map(it -> this.verify(it, clazz))
                 .filter(it -> it != Recipe.class)
                 .forEach(it -> handler.registerRecipeHandler(GenericUtil.uncheck(it), (IRecipeHandler<?>) InstantiationUtil.getOrCreateInstance(clazz)));
     }
     
+    private <T> Class<?> verify(final Class<T> type, final Class<?> target) {
+        
+        if(type == Recipe.class) {
+            throw new IllegalArgumentException("Class " + target.getName() + " wants to be a global handler: this is not allowed");
+        }
+        return type;
+    }
     
 }
