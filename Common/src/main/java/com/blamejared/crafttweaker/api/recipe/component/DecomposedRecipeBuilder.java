b38f6cb67fb19f6a62ca9cfa535bb6184dbe42e2
TheSilkMiner <thesilkminer@outlook.com>
2022-09-16 17:08:27 +0200

    Make decomposed recipe more robust
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- Common/src/main/java/com/blamejared/crafttweaker/api/recipe/component/DecomposedRecipeBuilder.java
+++ Common/src/main/java/com/blamejared/crafttweaker/api/recipe/component/DecomposedRecipeBuilder.java
@@ -2,7 +2,6 @@ package com.blamejared.crafttweaker.api.recipe.component;
 
 import com.blamejared.crafttweaker.api.util.GenericUtil;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,47 +10,53 @@ import java.util.Set;
 public final class DecomposedRecipeBuilder {
     
     private static final class SimpleDecomposedRecipe implements IDecomposedRecipe {
-    
+        
         private final Map<IRecipeComponent<?>, List<?>> components;
         
         SimpleDecomposedRecipe(final Map<IRecipeComponent<?>, List<?>> components) {
             
             this.components = new HashMap<>();
-            components.forEach((key, value) -> this.components.put(key, Collections.unmodifiableList(value)));
+            components.forEach((key, value) -> this.components.put(key, List.copyOf(value)));
         }
         
         @Override
         public <C> List<C> get(final IRecipeComponent<C> component) {
-        
+            
             return GenericUtil.uncheck(this.components.get(this.verify(component)));
         }
-    
+        
         @Override
         public <C> void set(final IRecipeComponent<C> component, final List<C> object) {
-        
-            this.components.put(this.verify(component), object);
+            
+            this.components.put(this.verify(component), List.copyOf(object));
         }
-    
+        
         @Override
         public Set<IRecipeComponent<?>> components() {
-        
+            
             return this.components.keySet();
         }
         
         private <C> IRecipeComponent<C> verify(final IRecipeComponent<C> component) {
             
-            return IRecipeComponent.find(component.id());
+            try {
+                return IRecipeComponent.find(component.id());
+            } catch(final IllegalArgumentException e) {
+                throw new IllegalStateException("Component " + component + " was not registered", e);
+            }
         }
-    
+        
     }
     
     private final Map<IRecipeComponent<?>, List<?>> components;
     
     private DecomposedRecipeBuilder() {
+        
         this.components = new HashMap<>();
     }
     
     static DecomposedRecipeBuilder of() {
+        
         return new DecomposedRecipeBuilder();
     }
     
@@ -65,4 +70,5 @@ public final class DecomposedRecipeBuilder {
         
         return new SimpleDecomposedRecipe(new HashMap<>(this.components));
     }
+    
 }
