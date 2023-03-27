71a91d25bd36f12fa56a1b02d5b997ef6d8ca9a9
Jared <jaredlll08@gmail.com>
2023-03-28 00:33:12 +0200

    Expose ShapedRecipe and ShapelessRecipe

---
+++ Common/src/main/java/com/blamejared/crafttweaker/natives/recipe/type/ExpandShapedRecipe.java
@@ -0,0 +1,44 @@
+package com.blamejared.crafttweaker.natives.recipe.type;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.ingredient.IIngredient;
+import com.blamejared.crafttweaker_annotations.annotations.*;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+import org.openzen.zencode.java.ZenCodeType;
+
+import java.util.stream.IntStream;
+
+@ZenRegister
+@Document("vanilla/api/recipe/type/ShapedRecipe")
+@NativeTypeRegistration(value = ShapedRecipe.class, zenCodeName = "crafttweaker.api.recipe.type.ShapedRecipe")
+public class ExpandShapedRecipe {
+    
+    /**
+     * Gets the ingredients of this recipe as an array that can be passed into a craftingTable.addShaped method call.
+     *
+     * @return the ingredients of this recipe as an array that can be passed into a craftingTable.addShaped method call.
+     */
+    @ZenCodeType.Method
+    public static IIngredient[][] getIngredientArray(ShapedRecipe internal) {
+        
+        return IntStream.range(0, internal.getHeight())
+                .mapToObj(y -> IntStream.range(0, internal.getWidth())
+                        .mapToObj(x -> internal.getIngredients().get(y * internal.getWidth() + x))
+                        .map(IIngredient::fromIngredient).toArray(IIngredient[]::new)).toArray(IIngredient[][]::new);
+    }
+    
+    @ZenCodeType.Method
+    @ZenCodeType.Getter("width")
+    public static int getWidth(ShapedRecipe internal) {
+        
+        return internal.getWidth();
+    }
+    
+    @ZenCodeType.Method
+    @ZenCodeType.Getter("height")
+    public static int getHeight(ShapedRecipe internal) {
+        
+        return internal.getHeight();
+    }
+    
+}
