add4e61cd2eaa630a5976525e50577d01df035c4
Jared <jaredlll08@gmail.com>
2023-05-21 17:16:50 +0200

    Add enumset import

--- Common/src/main/java/com/blamejared/crafttweaker/impl/script/scriptrun/ScriptRunManager.java
+++ Common/src/main/java/com/blamejared/crafttweaker/impl/script/scriptrun/ScriptRunManager.java
@@ -23,6 +23,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
