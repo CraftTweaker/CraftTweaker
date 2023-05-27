fdcd527703d42dfc32c39c4e807536e884c8ad62
TheSilkMiner <thesilkminer@outlook.com>
2023-05-27 16:39:29 +0200

    Provide access to the true cause of a bus exception
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- Common/src/main/java/com/blamejared/crafttweaker/api/event/bus/BusHandlingException.java
+++ Common/src/main/java/com/blamejared/crafttweaker/api/event/bus/BusHandlingException.java
@@ -2,9 +2,17 @@ package com.blamejared.crafttweaker.api.event.bus;
 
 public final class BusHandlingException extends RuntimeException {
     
+    private final Throwable original;
+    
     BusHandlingException(final Throwable t) {
         
         super("An error occurred while trying to dispatch an event onto the bus", t);
+        this.original = t instanceof BusHandlingException e? e.original() : t;
+    }
+    
+    public Throwable original() {
+        
+        return this.original;
     }
     
 }
