26708133681e4b984c05b152538545f070eaa200
TheSilkMiner <thesilkminer@outlook.com>
2023-06-12 11:14:52 +0200

    Disable loading of examples
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- Common/src/main/java/com/blamejared/crafttweaker/impl/command/type/script/example/ExamplesCommand.java
+++ Common/src/main/java/com/blamejared/crafttweaker/impl/command/type/script/example/ExamplesCommand.java
@@ -16,11 +16,90 @@ import org.openzen.zencode.shared.SourceFile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class ExamplesCommand {
+    
+    private static final class NoLoadInjectingInputStream extends InputStream {
+        private static final int EOF = -1;
+        
+        private static final byte INPUT_SOURCE_DELEGATE = 0;
+        private static final byte INPUT_SOURCE_BYTE = 1;
+        private static final byte INPUT_SOURCE_END = 2;
+        
+        private static final String INJECTED_STRING = """
+                #noload
+                // The line above will prevent the script from running, ensuring examples do not affect your current setup.
+                // Feel free to comment or remove the line if you want to see a particular example in action.
+                """;
+        
+        private final InputStream delegate;
+        private final ByteBuffer next;
+        
+        private boolean open;
+        private byte inputSource;
+        
+        NoLoadInjectingInputStream(final InputStream delegate, final Charset charset, final String lineEndings) {
+            this.delegate = delegate;
+            this.next = computeNext(charset, lineEndings);
+            this.open = true;
+            this.inputSource = INPUT_SOURCE_DELEGATE;
+        }
+        
+        private static ByteBuffer computeNext(final Charset charset, final String lineEndings) {
+            final String replaced = lineEndings + INJECTED_STRING.replace("\n", lineEndings);
+            final ByteBuffer encoded = charset.encode(replaced);
+            // Now we make a copy so that we can manipulate it at will, without any requirements from the underlying API
+            final ByteBuffer result = ByteBuffer.allocate(encoded.remaining());
+            result.put(encoded);
+            result.flip();
+            return result;
+        }
+        
+        @Override
+        public int read() throws IOException {
+            this.ensureOpen();
+            
+            if (this.inputSource == INPUT_SOURCE_DELEGATE) {
+                final int delegate = this.delegate.read();
+                if (delegate != EOF) {
+                    return delegate;
+                }
+                
+                this.inputSource = INPUT_SOURCE_BYTE;
+            }
+            
+            if (this.inputSource == INPUT_SOURCE_BYTE) {
+                try {
+                    return ((int) this.next.get()) & 0xFF;
+                } catch (final BufferUnderflowException e) {
+                    // Easier and likely faster to catch this once rather than checking every single time
+                    this.inputSource = INPUT_SOURCE_END;
+                }
+            }
+            
+            return EOF;
+        }
+        
+        @Override
+        public void close() throws IOException {
+            this.delegate.close();
+            this.next.clear();
+            this.open = false;
+        }
+        
+        private void ensureOpen() throws IOException {
+            if (!this.open) {
+                throw new IOException("Stream closed");
+            }
+        }
+        
+    }
     
     private ExamplesCommand() {}
     
@@ -70,7 +149,11 @@ public final class ExamplesCommand {
             }
         }
         
-        try(final Reader reader = sourceFile.open(); final InputStream stream = new ReaderInputStream(reader, StandardCharsets.UTF_8)) {
+        try(
+                final Reader reader = sourceFile.open();
+                final InputStream readerStream = new ReaderInputStream(reader, StandardCharsets.UTF_8);
+                final InputStream stream = new NoLoadInjectingInputStream(readerStream, StandardCharsets.UTF_8, "\n") // TODO("System-based?")
+        ) {
             Files.copy(stream, file);
         } catch(final IOException e) {
             CommandUtilities.COMMAND_LOGGER.warn("Could not write script example: ", e);
