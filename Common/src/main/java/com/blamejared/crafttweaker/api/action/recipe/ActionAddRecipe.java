9a00ec4fa325fcc27757b0af8355d3762df841a6
Sara Freimer <sara@freimer.com>
2023-06-29 11:11:28 -0500

    Expose method to change how outputs get described in the add action recipe

--- Common/src/main/java/com/blamejared/crafttweaker/api/action/recipe/ActionAddRecipe.java
+++ Common/src/main/java/com/blamejared/crafttweaker/api/action/recipe/ActionAddRecipe.java
@@ -3,25 +3,28 @@ package com.blamejared.crafttweaker.api.action.recipe;
 import com.blamejared.crafttweaker.api.recipe.manager.base.IRecipeManager;
 import com.blamejared.crafttweaker.api.util.ItemStackUtil;
 import com.blamejared.crafttweaker.impl.helper.AccessibleElementsProvider;
+
+import java.util.function.Function;
+
 import net.minecraft.world.item.crafting.Recipe;
 
 public class ActionAddRecipe<T extends Recipe<?>> extends ActionRecipeBase<T> {
     
     protected final T recipe;
     private final String subType;
+    private Function<T, String> describeOutputsFunction;
     
     public ActionAddRecipe(IRecipeManager<T> recipeManager, T recipe, String subType) {
         
         super(recipeManager);
         this.recipe = recipe;
         this.subType = subType;
+        this.describeOutputsFunction = r -> ItemStackUtil.getCommandString(AccessibleElementsProvider.get().registryAccess(r::getResultItem));
     }
     
     public ActionAddRecipe(IRecipeManager<T> recipeManager, T recipe) {
         
-        super(recipeManager);
-        this.recipe = recipe;
-        this.subType = "";
+        this(recipeManager, recipe, "");
     }
     
     @Override
@@ -33,12 +36,13 @@ public class ActionAddRecipe<T extends Recipe<?>> extends ActionRecipeBase<T> {
     @Override
     public String describe() {
         
-        return "Adding '%s' recipe%s, with name: '%s' that outputs: '%s'".formatted(getManager().getBracketResourceLocation(), getSubTypeDescription(), recipe.getId(), describeOutputs());
+        return "Adding '%s' recipe%s, with name: '%s' that outputs: '%s'".formatted(getManager().getBracketResourceLocation(), getSubTypeDescription(), recipe.getId(), describeOutputsFunction.apply(recipe));
     }
     
-    protected String describeOutputs() {
+    public ActionAddRecipe<T> outputDescriber(Function<T, String> describeOutputsFunction) {
         
-        return ItemStackUtil.getCommandString(AccessibleElementsProvider.get().registryAccess(recipe::getResultItem));
+        this.describeOutputsFunction = describeOutputsFunction;
+        return this;
     }
     
     private String getSubTypeDescription() {
