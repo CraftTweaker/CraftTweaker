d60aa082b05a023875b6a71ac8cc701722574f92
Jared <jaredlll08@gmail.com>
2023-07-12 16:57:02 -0600

    Expose player brewed potion event

---
+++ Forge/src/main/java/com/blamejared/crafttweaker/natives/event/brewing/ExpandPlayerBrewedPotionEvent.java
@@ -0,0 +1,31 @@
+package com.blamejared.crafttweaker.natives.event.brewing;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.event.ZenEvent;
+import com.blamejared.crafttweaker.api.event.bus.ForgeEventBusWire;
+import com.blamejared.crafttweaker.api.event.bus.IEventBus;
+import com.blamejared.crafttweaker.api.item.IItemStack;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraftforge.event.brewing.PlayerBrewedPotionEvent;
+import org.openzen.zencode.java.ZenCodeType;
+
+@ZenRegister
+@ZenEvent
+@Document("forge/api/event/brewing/PlayerBrewedPotionEvent")
+@NativeTypeRegistration(value = PlayerBrewedPotionEvent.class, zenCodeName = "crafttweaker.forge.api.event.brewing.PlayerBrewedPotionEvent")
+public class ExpandPlayerBrewedPotionEvent {
+    
+    @ZenEvent.Bus
+    public static final IEventBus<PlayerBrewedPotionEvent> BUS = IEventBus.direct(
+            PlayerBrewedPotionEvent.class,
+            ForgeEventBusWire.of()
+    );
+    
+    @ZenCodeType.Getter("stack")
+    public static IItemStack getStack(PlayerBrewedPotionEvent internal) {
+        
+        return IItemStack.of(internal.getStack());
+    }
+    
+}
