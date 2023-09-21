e211a13713f2740b1e92211c1399ff63fbdddd9a
Jared <jaredlll08@gmail.com>
2023-09-21 05:42:19 -0600

    Deprecated forge specific fluid tag expands and move them to common

--- Common/src/main/java/com/blamejared/crafttweaker/api/tag/expand/ExpandFluidTag.java
+++ Common/src/main/java/com/blamejared/crafttweaker/api/tag/expand/ExpandFluidTag.java
@@ -1,27 +1,32 @@
 package com.blamejared.crafttweaker.api.tag.expand;
 
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 import com.blamejared.crafttweaker.api.fluid.CTFluidIngredient;
 import com.blamejared.crafttweaker.api.tag.type.KnownTag;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
 import net.minecraft.world.level.material.Fluid;
+import org.openzen.zencode.java.ZenCodeType;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This expansion specifically targets FluidTags on Forge.
- *
- * @deprecated Use {@link ExpandFluidTag} instead.
+ * This expansion specifically targets FluidTags.
  */
-@Deprecated(forRemoval = true)
-public class ExpandFluidTagForge {
+@ZenRegister
+@Document("vanilla/api/tag/ExpandFluidTag")
+@ZenCodeType.Expansion("crafttweaker.api.tag.type.KnownTag<crafttweaker.api.fluid.Fluid>")
+public class ExpandFluidTag {
     
-    @Deprecated(forRemoval = true)
+    @ZenCodeType.Method
+    @ZenCodeType.Caster(implicit = true)
     public static CTFluidIngredient asFluidIngredient(KnownTag<Fluid> internal) {
         
         return new CTFluidIngredient.FluidTagWithAmountIngredient(internal.withAmount(1));
     }
     
-    @Deprecated(forRemoval = true)
+    @ZenCodeType.Method
+    @ZenCodeType.Operator(ZenCodeType.OperatorType.OR)
     public static CTFluidIngredient asList(KnownTag<Fluid> internal, CTFluidIngredient other) {
         
         List<CTFluidIngredient> elements = new ArrayList<>();
--- Common/src/main/java/com/blamejared/crafttweaker/api/tag/expand/ExpandManyFluidTag.java
+++ Common/src/main/java/com/blamejared/crafttweaker/api/tag/expand/ExpandManyFluidTag.java
@@ -1,26 +1,29 @@
 package com.blamejared.crafttweaker.api.tag.expand;
 
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 import com.blamejared.crafttweaker.api.fluid.CTFluidIngredient;
 import com.blamejared.crafttweaker.api.tag.type.KnownTag;
 import com.blamejared.crafttweaker.api.util.Many;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
 import net.minecraft.world.level.material.Fluid;
+import org.openzen.zencode.java.ZenCodeType;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * * @deprecated Use {@link ExpandManyFluidTag} instead.
- */
-@Deprecated(forRemoval = true)
-public class ExpandManyFluidTagForge {
+@ZenRegister
+@Document("vanilla/api/tag/ExpandManyFluidTag")
+@ZenCodeType.Expansion("crafttweaker.api.util.Many<crafttweaker.api.tag.type.KnownTag<crafttweaker.api.fluid.Fluid>>")
+public class ExpandManyFluidTag {
     
-    @Deprecated(forRemoval = true)
+    @ZenCodeType.Method
+    @ZenCodeType.Caster(implicit = true)
     public static CTFluidIngredient asFluidIngredient(Many<KnownTag<Fluid>> internal) {
         
         return new CTFluidIngredient.FluidTagWithAmountIngredient(internal);
     }
     
-    @Deprecated(forRemoval = true)
+    @ZenCodeType.Operator(ZenCodeType.OperatorType.OR)
     public static CTFluidIngredient asList(Many<KnownTag<Fluid>> internal, CTFluidIngredient other) {
         
         List<CTFluidIngredient> elements = new ArrayList<>();
