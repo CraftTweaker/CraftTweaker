75046d90d7b835e65c937ad96b0d89685ad5324e
Jared <jaredlll08@gmail.com>
2024-01-07 20:16:28 -0700

    Fix LootTableIdLootCondition not being usable

---
+++ Forge/src/main/java/com/blamejared/crafttweaker/natives/loot/condition/builder/ExpandLootTableIdConditionBuilder.java
@@ -0,0 +1,13 @@
+package com.blamejared.crafttweaker.natives.loot.condition.builder;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraftforge.common.loot.LootTableIdCondition;
+
+@ZenRegister
+@Document("forge/api/loot/condition/builder/LootTableIdConditionBuilder")
+@NativeTypeRegistration(value = LootTableIdCondition.Builder.class, zenCodeName = "crafttweaker.api.loot.condition.builder.LootTableIdConditionBuilder")
+public final class ExpandLootTableIdConditionBuilder {
+    
+}
