f8e7341b7b51f588c53d887af9eab98e888b8459
Jared <jaredlll08@gmail.com>
2025-03-02 03:51:40 -0700

    fix gradle

--- Common/build.gradle.kts
+++ Common/build.gradle.kts
@@ -1,10 +1,14 @@
 import com.blamejared.crafttweaker.gradle.Properties
 import com.blamejared.crafttweaker.gradle.Versions
 
+buildscript {
+    dependencies.add("classpath", "org.spongepowered:vanillagradle:0.2.1-20240507.024226-82")
+}
+
 plugins {
     id("com.blamejared.crafttweaker.default")
     id("com.blamejared.modtemplate")
-    id("org.spongepowered.gradle.vanilla") version "0.2.1-SNAPSHOT"
+    id("org.spongepowered.gradle.vanilla") version "0.2.1-20240507.024226-82"
 }
 
 minecraft {
