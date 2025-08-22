e78e820e5178facc659026e3322e96f1fddf3ee3
Jared <jaredlll08@gmail.com>
2025-08-22 00:34:05 -0600

    Fix ServerLevel getEntities requiring an entity type. Close #1816

--- Common/src/main/java/com/blamejared/crafttweaker/natives/entity/ExpandEntityTypeTest.java
+++ Common/src/main/java/com/blamejared/crafttweaker/natives/entity/ExpandEntityTypeTest.java
@@ -2,11 +2,24 @@ package com.blamejared.crafttweaker.natives.entity;
 
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.entity.EntityTypeTest;
 
 @ZenRegister
 //@Document("vanilla/api/entity/EntityTypeTest")
 @NativeTypeRegistration(value = EntityTypeTest.class, zenCodeName = "crafttweaker.api.entity.EntityTypeTest")
 public class ExpandEntityTypeTest {
-
+    
+    public static final EntityTypeTest<Entity, Entity> ANY = new EntityTypeTest<>() {
+        public Entity tryCast(Entity entity) {
+            
+            return entity;
+        }
+        
+        public Class<Entity> getBaseClass() {
+            
+            return Entity.class;
+        }
+    };
+    
 }
